f8d78089d2ecbda35343efdebe310eb8544cf60d
RealAnna <anna.reale@dynatrace.com>
2022-09-21 15:27:49 +0200

    removed useless event Signed-off-by: RealAnna <anna.reale@dynatrace.com>
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- lfc-scheduler/pkg/klcpermit/permit.go
+++ lfc-scheduler/pkg/klcpermit/permit.go
@@ -5,9 +5,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
-	ref "k8s.io/client-go/tools/reference"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
 	"time"
@@ -35,11 +33,6 @@ func (pl *Permit) Permit(ctx context.Context, state *framework.CycleState, p *v1
 
 	klog.InfoS("[Keptn Permit Plugin] waiting for pre-deployment checks on", p.GetObjectMeta().GetName())
 
-	err := pl.sendEvent(p, "test", "started", "Waiting for pre-deployment checks")
-	if err != nil {
-		return framework.NewStatus(framework.Error), 0 * time.Second
-	}
-
 	switch pl.svcManager.Permit(ctx, p) {
 
 	case Wait:
@@ -56,20 +49,6 @@ func (pl *Permit) Permit(ctx context.Context, state *framework.CycleState, p *v1
 		return framework.NewStatus(framework.Wait), 30 * time.Second //TODO what makes sense here?
 	}
 
-}
-
-func (pl *Permit) sendEvent(p *v1.Pod, reason string, action string, note string) error {
-	regarding, err := ref.GetPartialReference(scheme.Scheme, p, ".spec.containers[1]")
-	if err != nil {
-		return err
-	}
-
-	related, err := ref.GetPartialReference(scheme.Scheme, p, ".spec.containers[0]")
-	if err != nil {
-		return err
-	}
-	pl.handler.EventRecorder().Eventf(regarding, related, v1.EventTypeNormal, reason, action, note)
-	return nil
 }
 
 // New initializes a new plugin and returns it.

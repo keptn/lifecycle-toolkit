c97348be82f9d68a521376cf26b9ad2570be7aaf
Thomas Schuetz <thomas.schuetz@dynatrace.com>
2022-10-03 19:01:20 +0200

    fix(operator): update workload if spec changes
    Signed-off-by: Thomas Schuetz <thomas.schuetz@dynatrace.com>
    Signed-off-by: Giovanni Liva <giovanni.liva@dynatrace.com>

--- operator/webhooks/pod_mutating_webhook.go
+++ operator/webhooks/pod_mutating_webhook.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"reflect"
 	"strings"
 	"time"
 
@@ -113,6 +114,7 @@ func (a *PodMutatingWebhook) calculateVersion(pod *corev1.Pod) string {
 
 func (a *PodMutatingWebhook) handleWorkload(ctx context.Context, logger logr.Logger, pod *corev1.Pod, namespace string) error {
 	workloadName, _ := pod.Annotations[common.WorkloadAnnotation]
+	newWorkload := a.generateWorkload(pod, namespace)
 
 	workload := &klcv1alpha1.KeptnWorkload{}
 	err := a.Client.Get(ctx, types.NamespacedName{Namespace: namespace, Name: a.getWorkloadName(pod)}, workload)
@@ -120,7 +122,7 @@ func (a *PodMutatingWebhook) handleWorkload(ctx context.Context, logger logr.Log
 		logger.Info("Workload name", "workload", workloadName)
 
 		logger.Info("Creating workload workload", "workload", workload.Name)
-		workload = a.generateWorkload(pod, namespace)
+		workload = newWorkload
 		err = a.Client.Create(ctx, workload)
 		if err != nil {
 			logger.Error(err, "Could not create Workload")
@@ -140,13 +142,10 @@ func (a *PodMutatingWebhook) handleWorkload(ctx context.Context, logger logr.Log
 		return fmt.Errorf("could not fetch WorkloadInstance"+": %+v", err)
 	}
 
-	if workload.Spec.Version == pod.Annotations[common.VersionAnnotation] {
-		return nil
+	if !reflect.DeepEqual(workload.Spec, newWorkload.Spec) {
+		workload.Spec = newWorkload.Spec
 	}
 
-	workload.Spec.Version = pod.Annotations[common.VersionAnnotation]
-	workload.Spec.ResourceReference = a.getResourceReference(pod)
-	workload.Annotations[common.VersionAnnotation] = pod.Annotations[common.VersionAnnotation]
 	err = a.Client.Update(ctx, workload)
 	if err != nil {
 		logger.Error(err, "Could not update Workload")
@@ -189,9 +188,8 @@ func (a *PodMutatingWebhook) generateWorkload(pod *corev1.Pod, namespace string)
 
 	return &klcv1alpha1.KeptnWorkload{
 		ObjectMeta: metav1.ObjectMeta{
-			Annotations: pod.Annotations,
-			Name:        a.getWorkloadName(pod),
-			Namespace:   namespace,
+			Name:      a.getWorkloadName(pod),
+			Namespace: namespace,
 		},
 		Spec: klcv1alpha1.KeptnWorkloadSpec{
 			AppName:                applicationName,

279bc6aa9075392a043fdc110253cedc0820f2ae
Giovanni Liva <giovanni.liva@dynatrace.com>
2022-10-06 19:35:26 +0200

    Create a single span per deployment
    Signed-off-by: Giovanni Liva <giovanni.liva@dynatrace.com>

--- scheduler/pkg/klcpermit/workflow_manager.go
+++ scheduler/pkg/klcpermit/workflow_manager.go
@@ -3,6 +3,7 @@ package klcpermit
 import (
 	"context"
 	"github.com/keptn-sandbox/lifecycle-controller/scheduler/pkg/tracing"
+	"go.opentelemetry.io/otel/codes"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -53,9 +54,11 @@ func NewWorkloadManager(d dynamic.Interface) *WorkloadManager {
 	return sMgr
 }
 
+var bindCRDSpan = make(map[string]trace.Span, 100)
+
 func (sMgr *WorkloadManager) Permit(ctx context.Context, pod *corev1.Pod) Status {
 	//List workloadInstance run CRDs
-	name := GetCRDName(pod)
+	name := getCRDName(pod)
 	crd, err := sMgr.GetCRD(ctx, pod.Namespace, name)
 
 	if err != nil {
@@ -63,32 +66,30 @@ func (sMgr *WorkloadManager) Permit(ctx context.Context, pod *corev1.Pod) Status
 		return WorkloadInstanceNotFound
 	}
 
-	// search for annotations
-	annotations, found, err := unstructured.NestedMap(crd.UnstructuredContent(), "metadata", "annotations")
-	if found {
-		ctx = otel.GetTextMapPropagator().Extract(ctx, tracing.KeptnCarrier(annotations))
-	}
-
-	ctx, span := sMgr.Tracer.Start(ctx, "schedule")
-	defer span.End()
+	ctx, span := sMgr.getSpan(ctx, crd, pod)
 
 	//check CRD status
 	phase, found, err := unstructured.NestedString(crd.UnstructuredContent(), "status", "preDeploymentStatus")
 	klog.Infof("[Keptn Permit Plugin] workloadInstance crd %s, found %s with phase %s ", crd, found, phase)
 	if err == nil && found {
+		span.AddEvent("StatusEvaluation", trace.WithAttributes(tracing.Status.String(phase)))
 		switch KeptnState(phase) {
-		case StatePending:
-			return Wait
 		case StateFailed:
+			span.SetStatus(codes.Error, "Failed")
+			span.End()
+			unbindSpan(pod)
 			return Failure
 		case StateSucceeded:
+			span.End()
+			unbindSpan(pod)
 			return Success
+		case StatePending:
+			return Wait
 		case StateRunning:
 			return Wait
 		case StateUnknown:
 			return Wait
 		}
-
 	}
 	return WorkloadInstanceStatusNotSpecified
 }
@@ -99,9 +100,25 @@ func (sMgr *WorkloadManager) GetCRD(ctx context.Context, namespace string, name 
 	return sMgr.dynamicClient.Resource(workloadInstanceResource).Namespace(namespace).Get(ctx, name, metav1.GetOptions{})
 }
 
-func GetCRDName(pod *corev1.Pod) string {
+func (sMgr *WorkloadManager) getSpan(ctx context.Context, crd *unstructured.Unstructured, pod *corev1.Pod) (context.Context, trace.Span) {
+	name := getCRDName(pod)
+	if span, ok := bindCRDSpan[name]; ok {
+		return ctx, span
+	}
+	ctx, span := tracing.CreateSpan(ctx, crd, sMgr.Tracer, pod.Namespace)
+	//TODO store only sampled one and cap it
+	bindCRDSpan[name] = span
+	return ctx, span
+}
+
+func getCRDName(pod *corev1.Pod) string {
 	application := pod.Annotations["keptn.sh/app"]
 	workloadInstance := pod.Annotations["keptn.sh/workload"]
 	version := pod.Annotations["keptn.sh/version"]
 	return application + "-" + workloadInstance + "-" + version
+}
+
+func unbindSpan(pod *corev1.Pod) {
+	name := getCRDName(pod)
+	delete(bindCRDSpan, name)
 }
--- scheduler/pkg/tracing/semconv.go
+++ scheduler/pkg/tracing/semconv.go
@@ -2,21 +2,40 @@ package tracing
 
 import (
 	"context"
-	"github.com/keptn-sandbox/lifecycle-controller/scheduler/pkg/klcpermit"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
-func CreateSpan(ctx context.Context, crd unstructured.Unstructured, w klcpermit.WorkloadManager) (context.Context, trace.Span) {
+const (
+	ApplicationName attribute.Key = attribute.Key("keptn.deployment.app_name")
+	Workload        attribute.Key = attribute.Key("keptn.deployment.workload")
+	Version         attribute.Key = attribute.Key("keptn.deployment.version")
+	Namespace       attribute.Key = attribute.Key("keptn.deployment.namespace")
+	Status          attribute.Key = attribute.Key("keptn.deployment.status")
+)
+
+func CreateSpan(ctx context.Context, crd *unstructured.Unstructured, t trace.Tracer, ns string) (context.Context, trace.Span) {
 	// search for annotations
 	annotations, found, _ := unstructured.NestedMap(crd.UnstructuredContent(), "metadata", "annotations")
 	if found {
 		ctx = otel.GetTextMapPropagator().Extract(ctx, KeptnCarrier(annotations))
 	}
-	ctx, span := w.Tracer.Start(ctx, "schedule")
+	ctx, span := t.Start(ctx, "schedule")
 
-	// TODO extract from unstructured data Keptn Semantic Conventions
-
+	appName, found, _ := unstructured.NestedString(crd.UnstructuredContent(), "spec", "app")
+	if found {
+		span.SetAttributes(ApplicationName.String(appName))
+	}
+	workload, found, _ := unstructured.NestedString(crd.UnstructuredContent(), "spec", "workloadName")
+	if found {
+		span.SetAttributes(Workload.String(workload))
+	}
+	version, found, _ := unstructured.NestedString(crd.UnstructuredContent(), "spec", "version")
+	if found {
+		span.SetAttributes(Version.String(version))
+	}
+	span.SetAttributes(Namespace.String(ns))
 	return ctx, span
 }

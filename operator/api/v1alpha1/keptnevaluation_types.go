cd32c8f1a87eafcc03e965b082cc347d918682ee
odubajDT <ondrej.dubaj@dynatrace.com>
2022-10-14 11:00:22 +0200

    set up everything except logic
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- operator/api/v1alpha1/keptnevaluation_types.go
+++ operator/api/v1alpha1/keptnevaluation_types.go
@@ -20,6 +20,7 @@ import (
 	"time"
 
 	"github.com/keptn-sandbox/lifecycle-controller/operator/api/v1alpha1/common"
+	"go.opentelemetry.io/otel/attribute"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -46,6 +47,8 @@ type KeptnEvaluationStatus struct {
 	RetryCount       int                    `json:"retryCount"`
 	EvaluationStatus []EvaluationStatusItem `json:"evaluationStatus"`
 	OverallStatus    common.KeptnState      `json:"overallStatus"`
+	StartTime        metav1.Time            `json:"startTime,omitempty"`
+	EndTime          metav1.Time            `json:"endTime,omitempty"`
 }
 
 type EvaluationStatusItem struct {
@@ -78,4 +81,47 @@ type KeptnEvaluationList struct {
 
 func init() {
 	SchemeBuilder.Register(&KeptnEvaluation{}, &KeptnEvaluationList{})
+}
+
+func (i *KeptnEvaluation) SetStartTime() {
+	if i.Status.StartTime.IsZero() {
+		i.Status.StartTime = metav1.NewTime(time.Now().UTC())
+	}
+}
+
+func (i *KeptnEvaluation) SetEndTime() {
+	if i.Status.EndTime.IsZero() {
+		i.Status.EndTime = metav1.NewTime(time.Now().UTC())
+	}
+}
+
+func (i *KeptnEvaluation) IsStartTimeSet() bool {
+	return !i.Status.StartTime.IsZero()
+}
+
+func (i *KeptnEvaluation) IsEndTimeSet() bool {
+	return !i.Status.EndTime.IsZero()
+}
+
+func (i KeptnEvaluation) GetActiveMetricsAttributes() []attribute.KeyValue {
+	return []attribute.KeyValue{
+		common.AppName.String(i.Spec.AppName),
+		common.AppVersion.String(i.Spec.AppVersion),
+		common.WorkloadName.String(i.Spec.Workload),
+		common.WorkloadVersion.String(i.Spec.WorkloadVersion),
+		common.EvaluationName.String(i.Name),
+		common.EvaluationType.String(string(i.Spec.Type)),
+	}
+}
+
+func (i KeptnEvaluation) GetMetricsAttributes() []attribute.KeyValue {
+	return []attribute.KeyValue{
+		common.AppName.String(i.Spec.AppName),
+		common.AppVersion.String(i.Spec.AppVersion),
+		common.WorkloadName.String(i.Spec.Workload),
+		common.WorkloadVersion.String(i.Spec.WorkloadVersion),
+		common.EvaluationName.String(i.Name),
+		common.EvaluationType.String(string(i.Spec.Type)),
+		common.EvaluationStatus.String(string(i.Status.OverallStatus)),
+	}
 }

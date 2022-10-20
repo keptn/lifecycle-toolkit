f7a149fe1d174d40588b093f88c720aab4da3277
odubajDT <ondrej.dubaj@dynatrace.com>
2022-10-20 14:05:59 +0200

    pr review
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- operator/controllers/keptntask/job_utils.go
+++ operator/controllers/keptntask/job_utils.go
@@ -134,10 +134,16 @@ func (r *KeptnTaskReconciler) getJob(ctx context.Context, jobName string, namesp
 }
 
 func createKeptnLabels(task klcv1alpha1.KeptnTask) map[string]string {
+	if task.Spec.Workload != "" {
+		return map[string]string{
+			common.WorkloadAnnotation: task.Spec.Workload,
+			common.VersionAnnotation:  task.Spec.WorkloadVersion,
+			common.TaskNameAnnotation: task.Name,
+		}
+	}
 	return map[string]string{
 		common.AppAnnotation:      task.Spec.AppName,
-		common.WorkloadAnnotation: task.Spec.Workload,
-		common.VersionAnnotation:  task.Spec.WorkloadVersion,
+		common.VersionAnnotation:  task.Spec.AppVersion,
 		common.TaskNameAnnotation: task.Name,
 	}
 }

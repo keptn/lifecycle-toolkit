dbe08c0a5947a3fbe42aa94660352c3ef6357f14
RealAnna <89971034+RealAnna@users.noreply.github.com>
2022-11-07 16:02:02 +0100

    feat: Bootstrap webhook/component/integration/performance tests (#225)

--- operator/controllers/keptnapp/controller.go
+++ operator/controllers/keptnapp/controller.go
@@ -19,7 +19,7 @@ package keptnapp
 import (
 	"context"
 	"fmt"
-
+	"github.com/keptn/lifecycle-toolkit/operator/controllers/common"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
@@ -43,12 +43,13 @@ import (
 )
 
 // KeptnAppReconciler reconciles a KeptnApp object
+
 type KeptnAppReconciler struct {
 	client.Client
 	Scheme   *runtime.Scheme
 	Recorder record.EventRecorder
 	Log      logr.Logger
-	Tracer   trace.Tracer
+	Tracer   common.ITracer
 }
 
 //+kubebuilder:rbac:groups=lifecycle.keptn.sh,resources=keptnapps,verbs=get;list;watch;create;update;patch;delete
---
+++ operator/test/component/load_test.go
@@ -0,0 +1,118 @@
+package component
+
+import (
+	"fmt"
+	klcv1alpha1 "github.com/keptn/lifecycle-toolkit/operator/api/v1alpha1"
+	keptncontroller "github.com/keptn/lifecycle-toolkit/operator/controllers/common"
+	"github.com/keptn/lifecycle-toolkit/operator/controllers/keptnapp"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	otelsdk "go.opentelemetry.io/otel/sdk/trace"
+	sdktest "go.opentelemetry.io/otel/sdk/trace/tracetest"
+	"io/ioutil"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"log"
+	"os"
+	"path"
+	"time"
+)
+
+type Metric struct {
+	creationTime             []time.Time
+	succeededAppVersionCount int
+}
+
+const LOAD = 100
+
+var _ = Describe("[Feature:Performance] Load KeptnAppController", Ordered, func() {
+	var (
+		apps         []*klcv1alpha1.KeptnApp //Shelf is declared here
+		appVersions  []*klcv1alpha1.KeptnAppVersion
+		spanRecorder *sdktest.SpanRecorder
+		tracer       *otelsdk.TracerProvider
+		metrics      Metric
+	)
+	BeforeAll(func() {
+		//setup once
+		By("Waiting for Manager")
+		Eventually(func() bool {
+			return k8sManager != nil
+		}).Should(Equal(true))
+
+		spanRecorder = sdktest.NewSpanRecorder()
+		tracer = otelsdk.NewTracerProvider(otelsdk.WithSpanProcessor(spanRecorder))
+
+		controllers := []keptncontroller.Controller{&keptnapp.KeptnAppReconciler{
+			Client:   k8sManager.GetClient(),
+			Scheme:   k8sManager.GetScheme(),
+			Recorder: k8sManager.GetEventRecorderFor("load-app-controller"),
+			Log:      GinkgoLogr,
+			Tracer:   tracer.Tracer("load-app-tracer"),
+		}}
+		setupManager(controllers)
+	})
+
+	BeforeEach(func() {
+		//		createTimes := make(map[string]metav1.Time, 0)
+
+		for i := 0; i < LOAD; i++ {
+			instance := &klcv1alpha1.KeptnApp{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      fmt.Sprintf("app-%d", i),
+					Namespace: "default",
+				},
+				Spec: klcv1alpha1.KeptnAppSpec{
+					Version: "1.2.3",
+					Workloads: []klcv1alpha1.KeptnWorkloadRef{
+						{
+							Name:    "app-wname",
+							Version: "2.0",
+						},
+					},
+				},
+			}
+			apps = append(apps, instance)
+			Expect(k8sClient.Create(ctx, instance)).Should(Succeed())
+			metrics.creationTime = append(metrics.creationTime, time.Now())
+		}
+	})
+
+	AfterAll(func() {
+		generateMetricReport(metrics)
+	})
+	AfterEach(func() {
+		for i, app := range apps {
+			// Remember to clean up the cluster after each test
+			deleteAppInCluster(app)
+			deleteAppVersionInCluster(appVersions[i])
+			resetSpanRecords(tracer, spanRecorder)
+		}
+	})
+	JustAfterEach(func() { // this is an example of how to add logs to report
+		if CurrentSpecReport().Failed() {
+			AddReportEntry("current spans", spanRecorder.Ended())
+		}
+	})
+
+	It("should create the app version CR", func() {
+		for _, app := range apps {
+			appVersions = append(appVersions, assertResourceUpdated(app))
+			metrics.succeededAppVersionCount++
+		}
+	})
+})
+
+func generateMetricReport(metric Metric) {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dir)
+
+	filePath := path.Join(dir, "load-report."+time.Now().Format(time.RFC3339)+".json")
+	report := []byte(fmt.Sprintf("Overall AppVersions created %d/%d \n Creation times: %+v ", metric.succeededAppVersionCount, LOAD, metric.creationTime))
+	if err := ioutil.WriteFile(filePath, report, 0644); err != nil {
+		GinkgoLogr.Error(err, "error writing to ", filePath)
+	}
+
+}

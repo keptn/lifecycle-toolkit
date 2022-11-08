c675c5832f5cb603f7f18fff1af3b0b178a5c336
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-11-08 08:29:50 +0100

    add permission to list apps.v1 resources to operator
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- operator/webhooks/pod_mutating_webhook.go
+++ operator/webhooks/pod_mutating_webhook.go
@@ -32,7 +32,7 @@ import (
 
 // +kubebuilder:webhook:path=/mutate-v1-pod,mutating=true,failurePolicy=fail,groups="",resources=pods,verbs=create;update,versions=v1,name=mpod.keptn.sh,admissionReviewVersions=v1,sideEffects=None
 //+kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch
-//+kubebuilder:rbac:groups=apps,resources=deployments;statefulsets;daemonsets,verbs=get;list;watch
+//+kubebuilder:rbac:groups=apps,resources=deployments;statefulsets;daemonsets,verbs=list
 
 // PodMutatingWebhook annotates Pods
 type PodMutatingWebhook struct {
@@ -270,7 +270,7 @@ func (a *PodMutatingWebhook) copyResourceLabelsIfPresent(sourceResource *metav1.
 		if gotPreEvaluationChecks {
 			targetPod.Annotations[common.PreDeploymentEvaluationAnnotation] = preEvaluationChecks
 		}
-		
+
 		if gotPostEvaluationChecks {
 			targetPod.Annotations[common.PostDeploymentEvaluationAnnotation] = postEvaluationChecks
 		}

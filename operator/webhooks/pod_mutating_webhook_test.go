d529aa1ddc03c38978b2489ab839000af2b26542
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-11-08 13:09:17 +0100

    more tests
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- operator/webhooks/pod_mutating_webhook_test.go
+++ operator/webhooks/pod_mutating_webhook_test.go
@@ -32,8 +32,7 @@ func TestPodMutatingWebhook_getOwnerOfReplicaSet(t *testing.T) {
 		want   v1alpha1.ResourceReference
 	}{
 		{
-			name:   "Test simple return when UID and Kind is set",
-			fields: fields{},
+			name: "Test simple return when UID and Kind is set",
 			args: args{
 				rs: &appsv1.ReplicaSet{
 					ObjectMeta: metav1.ObjectMeta{
@@ -52,8 +51,7 @@ func TestPodMutatingWebhook_getOwnerOfReplicaSet(t *testing.T) {
 			},
 		},
 		{
-			name:   "Test return is input argument if owner is not found",
-			fields: fields{},
+			name: "Test return is input argument if owner is not found",
 			args: args{
 				rs: &appsv1.ReplicaSet{
 					TypeMeta: metav1.TypeMeta{
@@ -110,8 +108,7 @@ func TestPodMutatingWebhook_getReplicaSetOfPod(t *testing.T) {
 		want   v1alpha1.ResourceReference
 	}{
 		{
-			name:   "Test simple return when UID and Kind is set",
-			fields: fields{},
+			name: "Test simple return when UID and Kind is set",
 			args: args{
 				pod: &corev1.Pod{
 					ObjectMeta: metav1.ObjectMeta{
@@ -131,8 +128,7 @@ func TestPodMutatingWebhook_getReplicaSetOfPod(t *testing.T) {
 			},
 		},
 		{
-			name:   "Test return is input argument if owner is not found",
-			fields: fields{},
+			name: "Test return is input argument if owner is not found",
 			args: args{
 				pod: &corev1.Pod{
 					TypeMeta: metav1.TypeMeta{
@@ -166,6 +162,162 @@ func TestPodMutatingWebhook_getReplicaSetOfPod(t *testing.T) {
 			}
 			if got := a.getReplicaSetOfPod(tt.args.pod); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getReplicaSetOfPod() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPodMutatingWebhook_getAppName(t *testing.T) {
+	type fields struct {
+		Client   client.Client
+		Tracer   trace.Tracer
+		decoder  *admission.Decoder
+		Recorder record.EventRecorder
+		Log      logr.Logger
+	}
+	type args struct {
+		pod *corev1.Pod
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name: "Return keptn app name in lower case when annotation is set",
+			args: args{
+				pod: &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Annotations: map[string]string{
+							"keptn.sh/app": "SOME-APP-NAME",
+						},
+					},
+				},
+			},
+			want: "some-app-name",
+		},
+		{
+			name: "Return keptn app name in lower case when label is set",
+			args: args{
+				pod: &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{
+							"keptn.sh/app": "SOME-APP-NAME",
+						},
+					},
+				},
+			},
+			want: "some-app-name",
+		},
+		{
+			name: "Return keptn app name from annotation in lower case when annotation and label is set",
+			args: args{
+				pod: &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Annotations: map[string]string{
+							"keptn.sh/app": "SOME-APP-NAME-ANNOTATION",
+						},
+						Labels: map[string]string{
+							"keptn.sh/app": "SOME-APP-NAME-LABEL",
+						},
+					},
+				},
+			},
+			want: "some-app-name-annotation",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &PodMutatingWebhook{
+				Client:   tt.fields.Client,
+				Tracer:   tt.fields.Tracer,
+				decoder:  tt.fields.decoder,
+				Recorder: tt.fields.Recorder,
+				Log:      tt.fields.Log,
+			}
+			if got := a.getAppName(tt.args.pod); got != tt.want {
+				t.Errorf("getAppName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPodMutatingWebhook_getWorkloadName(t *testing.T) {
+	type fields struct {
+		Client   client.Client
+		Tracer   trace.Tracer
+		decoder  *admission.Decoder
+		Recorder record.EventRecorder
+		Log      logr.Logger
+	}
+	type args struct {
+		pod *corev1.Pod
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name: "Return concatenated app name and workload name in lower case when annotations are set",
+			args: args{
+				pod: &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Annotations: map[string]string{
+							"keptn.sh/app":      "SOME-APP-NAME",
+							"keptn.sh/workload": "SOME-WORKLOAD-NAME",
+						},
+					},
+				},
+			},
+			want: "some-app-name-some-workload-name",
+		},
+		{
+			name: "Return concatenated app name and workload name in lower case when labels are set",
+			args: args{
+				pod: &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{
+							"keptn.sh/app":      "SOME-APP-NAME",
+							"keptn.sh/workload": "SOME-WORKLOAD-NAME",
+						},
+					},
+				},
+			},
+			want: "some-app-name-some-workload-name",
+		},
+		{
+			name: "Return concatenated keptn app name and workload name from annotation in lower case when annotations and labels are set",
+			args: args{
+				pod: &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Annotations: map[string]string{
+							"keptn.sh/app":      "SOME-APP-NAME-ANNOTATION",
+							"keptn.sh/workload": "SOME-WORKLOAD-NAME-ANNOTATION",
+						},
+						Labels: map[string]string{
+							"keptn.sh/app":      "SOME-APP-NAME-LABEL",
+							"keptn.sh/workload": "SOME-WORKLOAD-NAME-LABEL",
+						},
+					},
+				},
+			},
+			want: "some-app-name-annotation-some-workload-name-annotation",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &PodMutatingWebhook{
+				Client:   tt.fields.Client,
+				Tracer:   tt.fields.Tracer,
+				decoder:  tt.fields.decoder,
+				Recorder: tt.fields.Recorder,
+				Log:      tt.fields.Log,
+			}
+			if got := a.getWorkloadName(tt.args.pod); got != tt.want {
+				t.Errorf("getWorkloadName() = %v, want %v", got, tt.want)
 			}
 		})
 	}

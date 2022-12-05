c4fa480acd6cc852d5b5761f315e37efb5c1c9e1
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-12-05 15:36:12 +0100

    set up hub and spoke conversion functions between api versions
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

---
+++ operator/api/v1alpha1/keptnapp_conversion.go
@@ -0,0 +1,57 @@
+package v1alpha1
+
+import (
+	"github.com/keptn/lifecycle-toolkit/operator/api/v1alpha2"
+	"sigs.k8s.io/controller-runtime/pkg/conversion"
+)
+
+func (src *KeptnApp) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*v1alpha2.KeptnApp)
+
+	// Copy equal stuff to new object
+	dst.TypeMeta = src.TypeMeta
+	dst.ObjectMeta = src.ObjectMeta
+
+	dst.Spec.Version = src.Spec.Version
+	for _, srcWl := range src.Spec.Workloads {
+		dst.Spec.Workloads = append(dst.Spec.Workloads, v1alpha2.KeptnWorkloadRef{
+			Name:    srcWl.Name,
+			Version: srcWl.Version,
+		})
+	}
+	dst.Spec.PreDeploymentTasks = src.Spec.PreDeploymentTasks
+	dst.Spec.PostDeploymentTasks = src.Spec.PostDeploymentTasks
+	dst.Spec.PreDeploymentEvaluations = src.Spec.PreDeploymentEvaluations
+	dst.Spec.PostDeploymentEvaluations = src.Spec.PostDeploymentEvaluations
+
+	dst.Status.CurrentVersion = src.Status.CurrentVersion
+
+	// Set sensible defaults for new fields
+	dst.Spec.Revision = "1"
+
+	return nil
+}
+
+func (dst *KeptnApp) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*v1alpha2.KeptnApp)
+
+	// Copy equal stuff to new object
+	dst.TypeMeta = src.TypeMeta
+	dst.ObjectMeta = src.ObjectMeta
+
+	dst.Spec.Version = src.Spec.Version
+	for _, srcWl := range src.Spec.Workloads {
+		dst.Spec.Workloads = append(dst.Spec.Workloads, KeptnWorkloadRef{
+			Name:    srcWl.Name,
+			Version: srcWl.Version,
+		})
+	}
+	dst.Spec.PreDeploymentTasks = src.Spec.PreDeploymentTasks
+	dst.Spec.PostDeploymentTasks = src.Spec.PostDeploymentTasks
+	dst.Spec.PreDeploymentEvaluations = src.Spec.PreDeploymentEvaluations
+	dst.Spec.PostDeploymentEvaluations = src.Spec.PostDeploymentEvaluations
+
+	dst.Status.CurrentVersion = src.Status.CurrentVersion
+
+	return nil
+}
---
+++ operator/api/v1alpha2/keptnapp_conversion.go
@@ -0,0 +1,3 @@
+package v1alpha2
+
+func (*KeptnApp) Hub() {}

b70ce13547bbe1695e61fc29369e1dcb5d7769b7
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-12-05 08:27:36 +0100

    fix import order
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- operator/controllers/common/evaluationhandler.go
+++ operator/controllers/common/evaluationhandler.go
@@ -3,10 +3,10 @@ package common
 import (
 	"context"
 	"fmt"
-	klcv1alpha2 "github.com/keptn/lifecycle-toolkit/operator/api/v1alpha2"
 	"time"
 
 	"github.com/go-logr/logr"
+	klcv1alpha2 "github.com/keptn/lifecycle-toolkit/operator/api/v1alpha2"
 	apicommon "github.com/keptn/lifecycle-toolkit/operator/api/v1alpha2/common"
 	controllererrors "github.com/keptn/lifecycle-toolkit/operator/controllers/errors"
 	"github.com/keptn/lifecycle-toolkit/operator/controllers/interfaces"

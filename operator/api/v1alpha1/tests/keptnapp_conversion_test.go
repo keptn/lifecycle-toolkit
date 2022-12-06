28ef6077bd83a986bbef983e03e5de7144f5ddba
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-12-06 10:15:09 +0100

    add unit tests
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- operator/api/v1alpha1/tests/keptnapp_conversion_test.go
+++ operator/api/v1alpha1/tests/keptnapp_conversion_test.go
@@ -2,72 +2,245 @@ package api
 
 import (
 	"github.com/keptn/lifecycle-toolkit/operator/api/v1alpha1"
+	"github.com/keptn/lifecycle-toolkit/operator/api/v1alpha2"
+	"github.com/stretchr/testify/require"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/conversion"
 	"testing"
 )
 
 func TestKeptnApp_ConvertFrom(t *testing.T) {
-	type fields struct {
-		TypeMeta   v1.TypeMeta
-		ObjectMeta v1.ObjectMeta
-		Spec       v1alpha1.KeptnAppSpec
-		Status     v1alpha1.KeptnAppStatus
-	}
-	type args struct {
-		srcRaw conversion.Hub
-	}
 	tests := []struct {
 		name    string
-		fields  fields
-		args    args
+		srcObj  *v1alpha2.KeptnApp
 		wantErr bool
+		wantObj *v1alpha1.KeptnApp
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test that conversion from v1alpha2 to v1alpha1 works",
+			srcObj: &v1alpha2.KeptnApp{
+				TypeMeta: v1.TypeMeta{
+					Kind:       "KeptnApp",
+					APIVersion: "lifecycle.keptn.sh/v1alpha2",
+				},
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "some-keptn-app-name",
+					Namespace: "",
+					Labels: map[string]string{
+						"some-label": "some-label-value",
+					},
+					Annotations: map[string]string{
+						"some-annotation": "some-annotation-value",
+					},
+				},
+				Spec: v1alpha2.KeptnAppSpec{
+					Version:  "1.2.3",
+					Revision: "1",
+					Workloads: []v1alpha2.KeptnWorkloadRef{
+						{
+							Name:    "workload-1",
+							Version: "1.2.3",
+						},
+						{
+							Name:    "workload-2",
+							Version: "4.5.6",
+						},
+					},
+					PreDeploymentTasks: []string{
+						"some-pre-deployment-task1",
+					},
+					PostDeploymentTasks: []string{
+						"some-post-deployment-task2",
+					},
+					PreDeploymentEvaluations: []string{
+						"some-pre-evaluation-task1",
+					},
+					PostDeploymentEvaluations: []string{
+						"some-pre-evaluation-task2",
+					},
+				},
+				Status: v1alpha2.KeptnAppStatus{
+					CurrentVersion: "1.2.3",
+				},
+			},
+			wantErr: false,
+			wantObj: &v1alpha1.KeptnApp{
+				TypeMeta: v1.TypeMeta{
+					Kind:       "KeptnApp",
+					APIVersion: "lifecycle.keptn.sh/v1alpha2",
+				},
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "some-keptn-app-name",
+					Namespace: "",
+					Labels: map[string]string{
+						"some-label": "some-label-value",
+					},
+					Annotations: map[string]string{
+						"some-annotation": "some-annotation-value",
+					},
+				},
+				Spec: v1alpha1.KeptnAppSpec{
+					Version: "1.2.3",
+					Workloads: []v1alpha1.KeptnWorkloadRef{
+						{
+							Name:    "workload-1",
+							Version: "1.2.3",
+						},
+						{
+							Name:    "workload-2",
+							Version: "4.5.6",
+						},
+					},
+					PreDeploymentTasks: []string{
+						"some-pre-deployment-task1",
+					},
+					PostDeploymentTasks: []string{
+						"some-post-deployment-task2",
+					},
+					PreDeploymentEvaluations: []string{
+						"some-pre-evaluation-task1",
+					},
+					PostDeploymentEvaluations: []string{
+						"some-pre-evaluation-task2",
+					},
+				},
+				Status: v1alpha1.KeptnAppStatus{
+					CurrentVersion: "1.2.3",
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			dst := &v1alpha1.KeptnApp{
-				TypeMeta:   tt.fields.TypeMeta,
-				ObjectMeta: tt.fields.ObjectMeta,
-				Spec:       tt.fields.Spec,
-				Status:     tt.fields.Status,
+				TypeMeta:   v1.TypeMeta{},
+				ObjectMeta: v1.ObjectMeta{},
+				Spec:       v1alpha1.KeptnAppSpec{},
+				Status:     v1alpha1.KeptnAppStatus{},
 			}
-			if err := dst.ConvertFrom(tt.args.srcRaw); (err != nil) != tt.wantErr {
+			if err := dst.ConvertFrom(tt.srcObj); (err != nil) != tt.wantErr {
 				t.Errorf("ConvertFrom() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.wantObj != nil {
+				require.Equal(t, tt.wantObj, dst, "Object was not converted correctly")
 			}
 		})
 	}
 }
 
 func TestKeptnApp_ConvertTo(t *testing.T) {
-	type fields struct {
-		TypeMeta   v1.TypeMeta
-		ObjectMeta v1.ObjectMeta
-		Spec       v1alpha1.KeptnAppSpec
-		Status     v1alpha1.KeptnAppStatus
-	}
-	type args struct {
-		dstRaw conversion.Hub
-	}
 	tests := []struct {
 		name    string
-		fields  fields
-		args    args
+		src     *v1alpha1.KeptnApp
 		wantErr bool
+		wantObj *v1alpha2.KeptnApp
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test that conversion from v1alpha1 to v1alpha2 works",
+			src: &v1alpha1.KeptnApp{
+				TypeMeta: v1.TypeMeta{
+					Kind:       "KeptnApp",
+					APIVersion: "lifecycle.keptn.sh/v1alpha2",
+				},
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "some-keptn-app-name",
+					Namespace: "",
+					Labels: map[string]string{
+						"some-label": "some-label-value",
+					},
+					Annotations: map[string]string{
+						"some-annotation": "some-annotation-value",
+					},
+				},
+				Spec: v1alpha1.KeptnAppSpec{
+					Version: "1.2.3",
+					Workloads: []v1alpha1.KeptnWorkloadRef{
+						{
+							Name:    "workload-1",
+							Version: "1.2.3",
+						},
+						{
+							Name:    "workload-2",
+							Version: "4.5.6",
+						},
+					},
+					PreDeploymentTasks: []string{
+						"some-pre-deployment-task1",
+					},
+					PostDeploymentTasks: []string{
+						"some-post-deployment-task2",
+					},
+					PreDeploymentEvaluations: []string{
+						"some-pre-evaluation-task1",
+					},
+					PostDeploymentEvaluations: []string{
+						"some-pre-evaluation-task2",
+					},
+				},
+				Status: v1alpha1.KeptnAppStatus{
+					CurrentVersion: "1.2.3",
+				},
+			},
+			wantErr: false,
+			wantObj: &v1alpha2.KeptnApp{
+				TypeMeta: v1.TypeMeta{
+					Kind:       "KeptnApp",
+					APIVersion: "lifecycle.keptn.sh/v1alpha2",
+				},
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "some-keptn-app-name",
+					Namespace: "",
+					Labels: map[string]string{
+						"some-label": "some-label-value",
+					},
+					Annotations: map[string]string{
+						"some-annotation": "some-annotation-value",
+					},
+				},
+				Spec: v1alpha2.KeptnAppSpec{
+					Version:  "1.2.3",
+					Revision: "1",
+					Workloads: []v1alpha2.KeptnWorkloadRef{
+						{
+							Name:    "workload-1",
+							Version: "1.2.3",
+						},
+						{
+							Name:    "workload-2",
+							Version: "4.5.6",
+						},
+					},
+					PreDeploymentTasks: []string{
+						"some-pre-deployment-task1",
+					},
+					PostDeploymentTasks: []string{
+						"some-post-deployment-task2",
+					},
+					PreDeploymentEvaluations: []string{
+						"some-pre-evaluation-task1",
+					},
+					PostDeploymentEvaluations: []string{
+						"some-pre-evaluation-task2",
+					},
+				},
+				Status: v1alpha2.KeptnAppStatus{
+					CurrentVersion: "1.2.3",
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			src := &v1alpha1.KeptnApp{
-				TypeMeta:   tt.fields.TypeMeta,
-				ObjectMeta: tt.fields.ObjectMeta,
-				Spec:       tt.fields.Spec,
-				Status:     tt.fields.Status,
+			dst := v1alpha2.KeptnApp{
+				TypeMeta:   v1.TypeMeta{},
+				ObjectMeta: v1.ObjectMeta{},
+				Spec:       v1alpha2.KeptnAppSpec{},
+				Status:     v1alpha2.KeptnAppStatus{},
 			}
-			if err := src.ConvertTo(tt.args.dstRaw); (err != nil) != tt.wantErr {
+			if err := tt.src.ConvertTo(&dst); (err != nil) != tt.wantErr {
 				t.Errorf("ConvertTo() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.wantObj != nil {
+				require.Equal(t, tt.wantObj, &dst, "Object was not converted correctly")
 			}
 		})
 	}

2d3e0e2db59a43a3c6e06246f948a9d5be3c23bb
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-12-07 12:43:43 +0100

    more tries
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- operator/main.go
+++ operator/main.go
@@ -349,6 +349,11 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&lifecyclev1alpha1.KeptnApp{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "KeptnApp")
+		os.Exit(1)
+	}
+
 	if err = (&lifecyclev1alpha2.KeptnApp{}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "KeptnApp")
 		os.Exit(1)

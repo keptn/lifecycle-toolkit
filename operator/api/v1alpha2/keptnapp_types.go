84878e93e36dec97f1bbccc14c17893004e7cb3e
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-12-13 11:31:26 +0100

    update default value to string
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- operator/api/v1alpha2/keptnapp_types.go
+++ operator/api/v1alpha2/keptnapp_types.go
@@ -31,7 +31,7 @@ import (
 // KeptnAppSpec defines the desired state of KeptnApp
 type KeptnAppSpec struct {
 	Version string `json:"version"`
-	// +kubebuilder:default:=1
+	// +kubebuilder:default:="1"
 	Revision                  string             `json:"revision,omitempty"`
 	Workloads                 []KeptnWorkloadRef `json:"workloads,omitempty"`
 	PreDeploymentTasks        []string           `json:"preDeploymentTasks,omitempty"`

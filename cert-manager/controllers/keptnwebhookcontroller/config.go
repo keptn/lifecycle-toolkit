7cd86da2bcaef37d320a87e78af0ffa1806b19f8
RealAnna <anna.reale@dynatrace.com>
2022-12-15 12:40:14 +0100

    feat: added base for cert-manager
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>
    
    feat: added base for cert-manager
    
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

---
+++ cert-manager/controllers/keptnwebhookcontroller/config.go
@@ -0,0 +1,8 @@
+package keptnwebhookcontroller
+
+const (
+
+	// DeploymentName is the name used for the Deployment of any webhooks and WebhookConfiguration objects.
+	DeploymentName = "klc-controller-manager"
+	ServiceName    = "klc-webhook-service"
+)

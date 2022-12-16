a474b06a253a6c1f4928ef1be4b4074f63278fd1
RealAnna <anna.reale@dynatrace.com>
2022-12-16 08:49:23 +0100

    feat: peer review, rename folder
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>
    
    rename
    
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>
    
    rename
    
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>
    
    feat: peer review
    
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- cert-manager/controllers/keptnwebhookcontroller/config.go
+++
@@ -1,8 +0,0 @@
-package keptnwebhookcontroller
-
-const (
-
-	// DeploymentName is the name used for the Deployment of any webhooks and WebhookConfiguration objects.
-	DeploymentName = "klc-controller-manager"
-	ServiceName    = "klc-webhook-service"
-)

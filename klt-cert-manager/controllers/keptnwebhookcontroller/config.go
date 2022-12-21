ea851ee8f6ffeed22e65180285e083e6a2806f4c
RealAnna <89971034+RealAnna@users.noreply.github.com>
2022-12-21 11:10:41 +0100

    Update klt-cert-manager/controllers/keptnwebhookcontroller/config.go
    Co-authored-by: odubajDT <93584209+odubajDT@users.noreply.github.com>
    Signed-off-by: RealAnna <89971034+RealAnna@users.noreply.github.com>

--- klt-cert-manager/controllers/keptnwebhookcontroller/config.go
+++ klt-cert-manager/controllers/keptnwebhookcontroller/config.go
@@ -1,7 +1,6 @@
 package keptnwebhookcontroller
 
 const (
-
 	// DeploymentName is the name used for the Deployment of any webhooks and WebhookConfiguration objects.
 	DeploymentName = "klc-controller-manager"
 	ServiceName    = "klc-webhook-service"

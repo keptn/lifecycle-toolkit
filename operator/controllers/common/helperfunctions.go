ea7597e40971f661067bcdbd58374fe50629b293
odubajDT <ondrej.dubaj@dynatrace.com>
2023-01-12 11:58:43 +0100

    recordEvent implemented
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- operator/controllers/common/helperfunctions.go
+++ operator/controllers/common/helperfunctions.go
@@ -27,6 +27,53 @@ func GetAppVersionName(namespace string, appName string, version string) types.N
 	return types.NamespacedName{Namespace: namespace, Name: appName + "-" + version}
 }
 
-func RecordEvent(recorder record.EventRecorder, phase apicommon.KeptnPhaseType, eventType string, reconcileObject client.Object, shortReason string, longReason string, version string) { //
-	recorder.Event(reconcileObject, eventType, fmt.Sprintf("%s %s", phase.ShortName, shortReason), fmt.Sprintf("%s %s / Namespace: %s, Name: %s, Version: %s ", phase.LongName, longReason, reconcileObject.GetNamespace(), reconcileObject.GetName(), version))
+func RecordEvent(recorder record.EventRecorder, phase apicommon.KeptnPhaseType, eventType string, reconcileObject client.Object, shortReason string, longReason string, version string) {
+	msg := fmt.Sprintf("%s %s / Namespace: %s, Name: %s, Version: %s ", phase.LongName, longReason, reconcileObject.GetNamespace(), reconcileObject.GetName(), version)
+	if version == "" {
+		msg = fmt.Sprintf("%s %s / Namespace: %s, Name: %s", phase.LongName, longReason, reconcileObject.GetNamespace(), reconcileObject.GetName())
+	}
+
+	annotations := map[string]string{
+		"namespace": reconcileObject.GetNamespace(),
+		"name":      reconcileObject.GetName(),
+		"phase":     phase.ShortName,
+	}
+	if app, ok := reconcileObject.(*klcv1alpha2.KeptnApp); ok {
+		annotations["appName"] = app.Name
+		annotations["appVersion"] = app.Spec.Version
+	} else if appVersion, ok := reconcileObject.(*klcv1alpha2.KeptnAppVersion); ok {
+		annotations["appName"] = appVersion.Spec.AppName
+		annotations["appVersion"] = app.Spec.Version
+		annotations["appVersionName"] = appVersion.Name
+	} else if workload, ok := reconcileObject.(*klcv1alpha2.KeptnWorkload); ok {
+		annotations["appName"] = workload.Spec.AppName
+		annotations["workloadName"] = workload.Spec.AppName
+		annotations["workloadVersion"] = workload.Spec.Version
+	} else if workloadInstance, ok := reconcileObject.(*klcv1alpha2.KeptnWorkloadInstance); ok {
+		annotations["appName"] = workloadInstance.Spec.AppName
+		annotations["workloadName"] = workloadInstance.Spec.WorkloadName
+		annotations["workloadVersion"] = workloadInstance.Spec.Version
+		annotations["workloadInstanceName"] = workloadInstance.Name
+	} else if task, ok := reconcileObject.(*klcv1alpha2.KeptnTask); ok {
+		annotations["appName"] = task.Spec.AppName
+		annotations["appVersion"] = task.Spec.AppVersion
+		annotations["workloadName"] = task.Spec.Workload
+		annotations["workloadVersion"] = task.Spec.WorkloadVersion
+		annotations["taskName"] = task.Name
+		annotations["taskDefinitionName"] = task.Spec.TaskDefinition
+	} else if evaluation, ok := reconcileObject.(*klcv1alpha2.KeptnEvaluation); ok {
+		annotations["appName"] = evaluation.Spec.AppName
+		annotations["appVersion"] = evaluation.Spec.AppVersion
+		annotations["workloadName"] = evaluation.Spec.Workload
+		annotations["workloadVersion"] = evaluation.Spec.WorkloadVersion
+		annotations["evaluationName"] = evaluation.Name
+		annotations["evaluationDefinitionName"] = evaluation.Spec.EvaluationDefinition
+	}
+
+	annotationsObject := reconcileObject.GetAnnotations()
+	if val, ok := annotationsObject["traceparent"]; ok {
+		annotations["traceId"] = val
+	}
+
+	recorder.AnnotatedEventf(reconcileObject, annotations, eventType, fmt.Sprintf("%s %s", phase.ShortName, shortReason), msg)
 }

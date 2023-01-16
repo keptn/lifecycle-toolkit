6f0276b14c218739e6953b9ef92f3e5368100ab7
RealAnna <anna.reale@dynatrace.com>
2023-01-16 10:28:00 +0100

    remove mistake changes
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- operator/cmd/certificates/watcher.go
+++ operator/cmd/certificates/watcher.go
@@ -11,12 +11,13 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/go-logr/logr"
-	"github.com/spf13/afero"
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	"github.com/go-logr/logr"
+	"github.com/spf13/afero"
 )
 
 // TODO: refactor code below to be testable and also tested

6715ad3dec7164115b82c291db069091e977ba92
RealAnna <anna.reale@dynatrace.com>
2023-01-17 17:26:10 +0100

    pr reviews
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- scheduler/test/e2e/scheduler_test.go
+++ scheduler/test/e2e/scheduler_test.go
@@ -8,7 +8,6 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/pkg/errors"
-
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"

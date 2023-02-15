e53b493dd118ff4b930cc9948967545267bc70c9
odubajDT <93584209+odubajDT@users.noreply.github.com>
2023-02-15 14:35:09 +0100

    feat(metrics-operator): introduce KeptnMetricsProvider CRD (#857)
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- metrics-operator/main.go
+++ metrics-operator/main.go
@@ -30,6 +30,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	metricsv1alpha2 "github.com/keptn/lifecycle-toolkit/metrics-operator/api/v1alpha2"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +43,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(metricsv1alpha2.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 

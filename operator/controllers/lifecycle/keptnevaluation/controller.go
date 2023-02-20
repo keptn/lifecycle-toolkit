972f4d24db00c380dd18432f5820e22a16f5061d
odubajDT <ondrej.dubaj@dynatrace.com>
2023-02-20 09:31:47 +0100

    adapt roles
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- operator/controllers/lifecycle/keptnevaluation/controller.go
+++ operator/controllers/lifecycle/keptnevaluation/controller.go
@@ -61,7 +61,7 @@ type KeptnEvaluationReconciler struct {
 //+kubebuilder:rbac:groups=lifecycle.keptn.sh,resources=keptnevaluationdefinitions,verbs=get;list;watch
 
 //role
-//+kubebuilder:rbac:groups=core,namespace=keptn-lifecycle-toolkit-system,resources=secrets,verbs=get
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.

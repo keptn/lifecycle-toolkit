e03f4eaabf379e0d87bf180c0ff781f5f224ab1c
Florian Bacher <florian.bacher@dynatrace.com>
2023-02-21 11:55:02 +0100

    remove unneeded logger
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- metrics-operator/api/v1alpha2/keptnmetric_webhook.go
+++ metrics-operator/api/v1alpha2/keptnmetric_webhook.go
@@ -18,11 +18,7 @@ package v1alpha2
 
 import (
 	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
-
-// log is for logging in this package.
-var keptnmetriclog = logf.Log.WithName("keptnmetric-resource")
 
 func (r *KeptnMetric) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).

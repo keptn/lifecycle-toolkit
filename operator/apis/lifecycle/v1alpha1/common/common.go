3cc969768a0edc182b19ce0fc6d7993165960e57
odubajDT <93584209+odubajDT@users.noreply.github.com>
2023-02-28 07:51:09 +0100

    Update operator/apis/lifecycle/v1alpha1/common/common.go
    Co-authored-by: Florian Bacher <florian.bacher@dynatrace.com>
    Signed-off-by: odubajDT <93584209+odubajDT@users.noreply.github.com>

--- operator/apis/lifecycle/v1alpha1/common/common.go
+++ operator/apis/lifecycle/v1alpha1/common/common.go
@@ -32,7 +32,7 @@ const MaxWorkloadNameLength = 25
 const MaxTaskNameLength = 25
 const MaxVersionLength = 12
 
-var ErrCannotCastKeptnEvaluationProvider = errors.New("cannot be cast KeptnEvaluationDefinition to v1alpha3")
+var ErrCannotCastKeptnEvaluationProvider = errors.New("cannot cast KeptnEvaluationDefinition to v1alpha3")
 var ErrCannotCastKeptnAppVersion = errors.New("cannot cast KeptnAppVersion to v1alpha3")
 var ErrCannotCastKeptnApp = errors.New("cannot cast KeptnApp to v1alpha3")
 var ErrCannotCastKeptnWorkloadInstance = errors.New("cannot cast KeptnWorkloadInstance to v1alpha3")

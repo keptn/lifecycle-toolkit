f2e33512774d5f78e7c25c6a01ef0cd390a25b7f
Florian Bacher <florian.bacher@dynatrace.com>
2023-03-28 07:49:02 +0200

    added simple kuttl and unit test to make sure the CRD can be applied
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- operator/controllers/lifecycle/keptnappcreationrequest/controller.go
+++ operator/controllers/lifecycle/keptnappcreationrequest/controller.go
@@ -41,7 +41,6 @@ type KeptnAppCreationRequestReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
 // the KeptnAppCreationRequest object against the actual cluster state, and then
 // perform operations to make the cluster state reflect the state specified by
 // the user.
@@ -50,8 +49,6 @@ type KeptnAppCreationRequestReconciler struct {
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *KeptnAppCreationRequestReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
-
-	// TODO(user): your logic here
 
 	return ctrl.Result{}, nil
 }

4bb08b9332b5470723d5677e5e2372e7f72252ef
realanna <anna.reale@dynatrace.com>
2023-04-20 09:12:21 +0200

    remove TODOS
    Signed-off-by: realanna <anna.reale@dynatrace.com>

--- operator/cmd/certificates/watcher.go
+++ operator/cmd/certificates/watcher.go
@@ -16,7 +16,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// TODO: refactor code below to be testable and also tested: https://github.com/keptn/lifecycle-toolkit/issues/640
 const (
 	certificateRenewalInterval = 6 * time.Hour
 	ServerKey                  = "tls.key"

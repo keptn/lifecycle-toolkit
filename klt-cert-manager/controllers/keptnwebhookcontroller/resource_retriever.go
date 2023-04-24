d8dbd8801d7dc6588cdd4b3122c98a2f01384ca4
Florian Bacher <florian.bacher@dynatrace.com>
2023-04-24 12:25:20 +0200

    added implementation for resource retriever
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- klt-cert-manager/controllers/keptnwebhookcontroller/resource_retriever.go
+++ klt-cert-manager/controllers/keptnwebhookcontroller/resource_retriever.go
@@ -2,10 +2,91 @@ package keptnwebhookcontroller
 
 import (
 	"context"
+	"github.com/go-logr/logr"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	apiv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type IResourceRetriever interface {
-	GetMutatingWebhooks(ctx context.Context)
-	GetValidatingWebhooks(ctx context.Context)
-	GetCRDs(ctx context.Context)
+	GetMutatingWebhooks(ctx context.Context) (*admissionregistrationv1.MutatingWebhookConfigurationList, error)
+	GetValidatingWebhooks(ctx context.Context) (*admissionregistrationv1.ValidatingWebhookConfigurationList, error)
+	GetCRDs(ctx context.Context) (*apiv1.CustomResourceDefinitionList, error)
+}
+
+type LabelSelectorRetriever struct {
+	MatchLabels labels.Set
+	Client      client.Client
+}
+
+func (r LabelSelectorRetriever) GetMutatingWebhooks(ctx context.Context) (*admissionregistrationv1.MutatingWebhookConfigurationList, error) {
+	var mutatingWebhooks admissionregistrationv1.MutatingWebhookConfigurationList
+
+	if err := r.Client.List(ctx, &mutatingWebhooks, client.MatchingLabels(r.MatchLabels)); err != nil {
+		return nil, err
+	}
+	return &mutatingWebhooks, nil
+}
+
+func (r LabelSelectorRetriever) GetValidatingWebhooks(ctx context.Context) (*admissionregistrationv1.ValidatingWebhookConfigurationList, error) {
+	var validatingWebhooks admissionregistrationv1.ValidatingWebhookConfigurationList
+
+	if err := r.Client.List(ctx, &validatingWebhooks, client.MatchingLabels(r.MatchLabels)); err != nil {
+		return nil, err
+	}
+	return &validatingWebhooks, nil
+}
+
+func (r LabelSelectorRetriever) GetCRDs(ctx context.Context) (*apiv1.CustomResourceDefinitionList, error) {
+	var crds apiv1.CustomResourceDefinitionList
+	opt := client.MatchingLabels(r.MatchLabels)
+	if err := r.Client.List(ctx, &crds, opt); err != nil {
+		return nil, err
+	}
+
+	return &crds, nil
+}
+
+type ResourceNameRetriever struct {
+	Client         client.Client
+	WatchResources ObservedObjects
+	Log            logr.Logger
+}
+
+func (r ResourceNameRetriever) GetMutatingWebhooks(ctx context.Context) (*admissionregistrationv1.MutatingWebhookConfigurationList, error) {
+	result := &admissionregistrationv1.MutatingWebhookConfigurationList{
+		Items: make([]admissionregistrationv1.MutatingWebhookConfiguration, len(r.WatchResources.MutatingWebhooks)),
+	}
+	for _, mwhName := range r.WatchResources.MutatingWebhooks {
+		mwh := &admissionregistrationv1.MutatingWebhookConfiguration{}
+		if err := r.Client.Get(ctx, client.ObjectKey{Name: mwhName}, mwh); err != nil {
+			r.Log.Error(err, "Could not retrieve MutatingWebhookConfiguration", "MutatingWebhookConfiguration", mwhName)
+		} else {
+			result.Items = append(result.Items, *mwh)
+		}
+	}
+
+	return result, nil
+}
+
+func (r ResourceNameRetriever) GetValidatingWebhooks(ctx context.Context) (*admissionregistrationv1.ValidatingWebhookConfigurationList, error) {
+	result := &admissionregistrationv1.ValidatingWebhookConfigurationList{
+		Items: make([]admissionregistrationv1.ValidatingWebhookConfiguration, len(r.WatchResources.ValidatingWebhooks)),
+	}
+	for _, vwhName := range r.WatchResources.ValidatingWebhooks {
+		vwh := &admissionregistrationv1.ValidatingWebhookConfiguration{}
+		if err := r.Client.Get(ctx, client.ObjectKey{Name: vwhName}, vwh); err != nil {
+			r.Log.Error(err, "Could not retrieve ValidatingWebhookConfiguration", "ValidatingWebhookConfiguration", vwhName)
+		} else {
+			result.Items = append(result.Items, *vwh)
+		}
+	}
+
+	return result, nil
+}
+
+func (r ResourceNameRetriever) GetCRDs(ctx context.Context) (*apiv1.CustomResourceDefinitionList, error) {
+	//TODO implement me
+	panic("implement me")
 }

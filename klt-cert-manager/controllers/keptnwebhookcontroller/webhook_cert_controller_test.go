c243dfd57ee65938ee9c47d6600fbb3987b5920e
Florian Bacher <florian.bacher@dynatrace.com>
2023-04-24 15:53:03 +0200

    adapted unit tests
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- klt-cert-manager/controllers/keptnwebhookcontroller/webhook_cert_controller_test.go
+++ klt-cert-manager/controllers/keptnwebhookcontroller/webhook_cert_controller_test.go
@@ -13,7 +13,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apiv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -410,12 +409,12 @@ func createTestSecret(_ *testing.T, certData map[string][]byte) *corev1.Secret {
 }
 
 func prepareController(t *testing.T, clt client.Client) (*KeptnWebhookCertificateReconciler, reconcile.Request) {
-	rec := &KeptnWebhookCertificateReconciler{
+	rec := NewReconciler(CertificateReconcilerConfig{
+		MatchLabels: map[string]string{"app.kubernetes.io/part-of": "keptn-lifecycle-toolkit"},
 		Client:      clt,
 		Log:         testr.New(t),
 		Namespace:   testnamespace,
-		MatchLabels: labels.Set(map[string]string{"app.kubernetes.io/part-of": "keptn-lifecycle-toolkit"}),
-	}
+	})
 
 	request := reconcile.Request{
 		NamespacedName: types.NamespacedName{

80f4b585944c7999b827956edc4068114abfcfb3
odubajDT <ondrej.dubaj@dynatrace.com>
2023-05-10 09:15:11 +0200

    working running scripts from url
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- python-runtime/run_script.sh
+++ python-runtime/run_script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-python3 $CMD_ARGS $SCRIPT 
+regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
+if [[ $SCRIPT =~ $regex ]]
+then
+    curl $SCRIPT | python3 $CMD_ARGS -
+else
+    python3 $CMD_ARGS $SCRIPT
+fi

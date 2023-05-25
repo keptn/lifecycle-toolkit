d2433f02bce1bac102fb3e06a76ffbe2141ee348
realanna <anna.reale@dynatrace.com>
2023-05-25 12:09:27 +0200

    testerrorr
    Signed-off-by: realanna <anna.reale@dynatrace.com>

--- metrics-operator/controllers/common/providers/datadog/datadog_test.go
+++ metrics-operator/controllers/common/providers/datadog/datadog_test.go
@@ -19,7 +19,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
 )
 
 const ddErrorPayload = "{\"error\":\"Token is missing required scope\"}"

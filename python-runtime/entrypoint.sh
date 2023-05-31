c49aec01f5d11d573ad24ec876df974f40eb2888
realanna <anna.reale@dynatrace.com>
2023-05-31 11:39:21 +0200

    fix: lint
    Signed-off-by: realanna <anna.reale@dynatrace.com>

--- python-runtime/entrypoint.sh
+++ python-runtime/entrypoint.sh
@@ -6,7 +6,7 @@ regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]
 
 if [[ $SCRIPT =~ $regex ]]
 then
-    curl $SCRIPT | python3 $CMD_ARGS -
+    curl -s $SCRIPT | python3 $CMD_ARGS -
 else
     python3 $CMD_ARGS $SCRIPT
 fi

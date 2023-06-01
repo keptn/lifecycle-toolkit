5e190e8c07dfe26264038d76b64e9afae6dc2e94
Giovanni Liva <giovanni.liva@dynatrace.com>
2023-06-01 08:49:37 +0200

    add handy script
    Signed-off-by: Giovanni Liva <giovanni.liva@dynatrace.com>

---
+++ .github/scripts/generate-latest-doc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Keptn Lifecycle Toolkit Documentation generation
+#
+# This script support the release of the latest version of the documentation
+# The final files are available unde the docs/release folder
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=${SCRIPT_DIR/.github\/scripts/}
+DOC_DIR="${ROOT_DIR}docs"
+WORK_DIR=${DOC_DIR}/release
+
+## Create the working folder
+mkdir -p $WORK_DIR
+
+cd $WORK_DIR
+git clone https://github.com/keptn/lifecycle-toolkit.git
+cd lifecycle-toolkit
+git checkout page
+git pull
+
+cp -r "${DOC_DIR}/content/en/docs" ./docs/content/en
+

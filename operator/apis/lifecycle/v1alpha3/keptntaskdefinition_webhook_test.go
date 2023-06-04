fdff5892763833f8e827acbc119334c5e513dea9
geoffrey1330 <israelgeoffrey13@gmail.com>
2023-06-04 14:00:50 +0100

    Added testcase keptntaskdefinition_webhook_test.go for keptntaskdefinition_webhook.go Signed-off-by: geoffrey1330 <israelgeoffrey13@gmail.com>

---
+++ operator/apis/lifecycle/v1alpha3/keptntaskdefinition_webhook_test.go
@@ -0,0 +1,66 @@
+package v1alpha3
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/pkg/errors"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+func TestKeptnTaskDefinition_ValidateFields(t *testing.T) {
+	tests := []struct {
+		name string
+		spec KeptnTaskDefinitionSpec
+		want *field.Error
+	}{
+		{
+			name: "with-no-function-or-container",
+			spec: KeptnTaskDefinitionSpec{},
+			want: field.Invalid(
+				field.NewPath("spec"),
+				KeptnTaskDefinitionSpec{},
+				errors.New("Forbidden! Either Function or Container field must be defined").Error(),
+			),
+		},
+		{
+			name: "with-both-function-and-container",
+			spec: KeptnTaskDefinitionSpec{
+				Function:  &FunctionSpec{},
+				Container: &ContainerSpec{},
+			},
+			want: field.Invalid(
+				field.NewPath("spec"),
+				KeptnTaskDefinitionSpec{
+					Function:  &FunctionSpec{},
+					Container: &ContainerSpec{},
+				},
+				errors.New("Forbidden! Both Function and Container fields cannot be defined simultaneously").Error(),
+			),
+		},
+		{
+			name: "with-function-only",
+			spec: KeptnTaskDefinitionSpec{
+				Function: &FunctionSpec{},
+			},
+		},
+		{
+			name: "with-container-only",
+			spec: KeptnTaskDefinitionSpec{
+				Container: &ContainerSpec{},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ktd := &KeptnTaskDefinition{
+				Spec: tt.spec,
+			}
+			if got := ktd.validateFields(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("validateFields() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

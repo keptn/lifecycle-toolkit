f0fca24c7b85a2b8055adbfd40fb4044f7929620
Florian Bacher <florian.bacher@dynatrace.com>
2023-07-05 14:34:43 +0200

    adapt integration/component test
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- operator/test/component/task/task_test.go
+++ operator/test/component/task/task_test.go
@@ -48,7 +48,7 @@ var _ = Describe("Task", Ordered, func() {
 					}, task)
 					g.Expect(err).To(BeNil())
 					g.Expect(task.Status.JobName).To(Not(BeEmpty()))
-				}, "10s").Should(Succeed())
+				}, "20s").Should(Succeed())
 
 				createdJob := &batchv1.Job{}
 
@@ -101,7 +101,7 @@ var _ = Describe("Task", Ordered, func() {
 					}, task)
 					g.Expect(err).To(BeNil())
 					g.Expect(task.Status.JobName).To(Not(BeEmpty()))
-				}, "10s").Should(Succeed())
+				}, "20s").Should(Succeed())
 
 				createdJob := &batchv1.Job{}
 
@@ -154,7 +154,7 @@ var _ = Describe("Task", Ordered, func() {
 					}, task)
 					g.Expect(err).To(BeNil())
 					g.Expect(task.Status.JobName).To(Not(BeEmpty()))
-				}, "10s").Should(Succeed())
+				}, "20s").Should(Succeed())
 
 				createdJob := &batchv1.Job{}
 
@@ -182,7 +182,7 @@ var _ = Describe("Task", Ordered, func() {
 					}, task)
 					g.Expect(err).To(BeNil())
 					g.Expect(task.Status.Status).To(Equal(apicommon.StateSucceeded))
-				}, "10s").Should(Succeed())
+				}, "20s").Should(Succeed())
 			})
 			It("should propagate labels and annotations to the job and job pod", func() {
 				taskDefinition = makeTaskDefinition(taskDefinitionName, namespace)
@@ -197,7 +197,7 @@ var _ = Describe("Task", Ordered, func() {
 					}, task)
 					g.Expect(err).To(BeNil())
 					g.Expect(task.Status.JobName).To(Not(BeEmpty()))
-				}, "10s").Should(Succeed())
+				}, "20s").Should(Succeed())
 
 				createdJob := &batchv1.Job{}
 

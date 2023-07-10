3828f1b789a5c6d4493c01ce2c9574c5cf0b683e
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2023-07-10 12:41:50 +0530

    feat: add `interval` field for `kubectl get KeptnMetric` (#1689)
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>
    Signed-off-by: Nitishupkr <nitishupkr@gmail.com>

--- metrics-operator/api/v1alpha3/keptnmetric_types.go
+++ metrics-operator/api/v1alpha3/keptnmetric_types.go
@@ -64,6 +64,7 @@ type RangeSpec struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Provider",type=string,JSONPath=`.spec.provider.name`
 // +kubebuilder:printcolumn:name="Query",type=string,JSONPath=`.spec.query`
+// +kubebuilder:printcolumn:name="Interval",type=string,JSONPath=`.spec.range.interval`
 // +kubebuilder:printcolumn:name="Value",type=string,JSONPath=`.status.value`
 // +kubebuilder:storageversion
 

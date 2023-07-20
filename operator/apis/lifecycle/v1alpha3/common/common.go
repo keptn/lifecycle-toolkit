f47ca50c1e34548466f9368c260c533e293754ad
Florian Bacher <florian.bacher@dynatrace.com>
2023-07-20 14:51:25 +0200

    fix: admit pod without creating KLT resources if owner of the pod is not supported (#1752)
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- operator/apis/lifecycle/v1alpha3/common/common.go
+++ operator/apis/lifecycle/v1alpha3/common/common.go
@@ -9,6 +9,7 @@ import (
 	operatorcommon "github.com/keptn/lifecycle-toolkit/operator/common"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const WorkloadAnnotation = "keptn.sh/workload"
@@ -199,4 +200,9 @@ func MergeMaps(m1 map[string]string, m2 map[string]string) map[string]string {
 		merged[key] = value
 	}
 	return merged
+}
+
+// IsOwnerSupported returns whether the owner of the given object is supported to be considered a KeptnWorklooad
+func IsOwnerSupported(owner metav1.OwnerReference) bool {
+	return owner.Kind == "ReplicaSet" || owner.Kind == "Deployment" || owner.Kind == "StatefulSet" || owner.Kind == "DaemonSet" || owner.Kind == "Rollout"
 }

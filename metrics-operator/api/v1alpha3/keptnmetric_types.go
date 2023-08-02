03ff517df86a2b465601186ab4c7a30bc8f2ba57
Florian Bacher <florian.bacher@dynatrace.com>
2023-08-02 08:53:09 +0200

    test again
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- metrics-operator/api/v1alpha3/keptnmetric_types.go
+++ metrics-operator/api/v1alpha3/keptnmetric_types.go
@@ -33,6 +33,8 @@ type KeptnMetricSpec struct {
 	FetchIntervalSeconds uint `json:"fetchIntervalSeconds"`
 	// Range represents the time range for which data is to be queried
 	Range *RangeSpec `json:"range,omitempty"`
+	// Foo is a new field
+	Foo string `json:"foo"`
 }
 
 // KeptnMetricStatus defines the observed state of KeptnMetric

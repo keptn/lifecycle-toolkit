adf4621c2e8147bc0e4ee7f1719859007290c978
odubajDT <93584209+odubajDT@users.noreply.github.com>
2023-08-08 14:31:25 +0200

    feat(metrics-operator): add AnalysisDefinition CRD (#1823)
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

---
+++ metrics-operator/api/v1alpha3/analysisdefinition_webhook.go
@@ -0,0 +1,130 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha3
+
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+)
+
+// log is for logging in this package.
+var analysisdefinitionlog = logf.Log.WithName("analysisdefinition-webhook")
+
+func (r *AnalysisDefinition) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
+
+//+kubebuilder:webhook:path=/validate-metrics-keptn-sh-v1alpha3-analysisdefinition,mutating=false,failurePolicy=fail,sideEffects=None,groups=metrics.keptn.sh,resources=analysisdefinitions,verbs=create;update,versions=v1alpha3,name=vanalysisdefinition.kb.io,admissionReviewVersions=v1
+
+var _ webhook.Validator = &AnalysisDefinition{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *AnalysisDefinition) ValidateCreate() error {
+	analysisdefinitionlog.Info("validate create", "name", r.Name)
+
+	for _, o := range r.Spec.Objectives {
+		if err := o.validate(); err != nil {
+			return err
+		}
+	}
+
+	return r.Spec.TotalScore.validate()
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *AnalysisDefinition) ValidateUpdate(old runtime.Object) error {
+	analysisdefinitionlog.Info("validate update", "name", r.Name)
+
+	for _, o := range r.Spec.Objectives {
+		if err := o.validate(); err != nil {
+			return err
+		}
+	}
+
+	return r.Spec.TotalScore.validate()
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *AnalysisDefinition) ValidateDelete() error {
+	analysisdefinitionlog.Info("validate delete", "name", r.Name)
+
+	return nil
+}
+
+func (s *TotalScore) validate() error {
+	if s.WarningPercentage >= s.PassPercentage {
+		return fmt.Errorf("Warn percentage score cannot be higher or equal than Pass percentage score")
+	}
+	return nil
+}
+
+func (o *Objective) validate() error {
+	if err := o.Target.validate(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *Target) validate() error {
+	if t.Failure == nil && t.Warning != nil {
+		return fmt.Errorf("Warning criteria cannot be set without Failure criteria")
+	}
+	if t.Failure != nil {
+		if err := t.Failure.validate(); err != nil {
+			return err
+		}
+	}
+
+	if t.Warning != nil {
+		if err := t.Warning.validate(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (o *Operator) validate() error {
+	counter := 0
+	if o.LessThan != nil {
+		counter++
+	}
+	if o.LessThanOrEqual != nil {
+		counter++
+	}
+	if o.GreaterThan != nil {
+		counter++
+	}
+	if o.GreaterThanOrEqual != nil {
+		counter++
+	}
+	if o.EqualTo != nil {
+		counter++
+	}
+	if counter > 1 {
+		return fmt.Errorf("Operator: multiple operators can not be set")
+	}
+	if counter == 0 {
+		return fmt.Errorf("Operator: no operator set")
+	}
+	return nil
+}
---
+++ metrics-operator/api/v1alpha3/analysisdefinition_webhook_test.go
@@ -0,0 +1,314 @@
+package v1alpha3
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+func TestOperator_validate(t *testing.T) {
+	tests := []struct {
+		name     string
+		operator Operator
+		wantErr  error
+	}{
+		{
+			name:     "no operator set",
+			operator: Operator{},
+			wantErr:  fmt.Errorf("Operator: no operator set"),
+		},
+		{
+			name: "multiple operators set",
+			operator: Operator{
+				LessThanOrEqual: &OperatorValue{
+					FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+				},
+				LessThan: &OperatorValue{
+					FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+				},
+			},
+			wantErr: fmt.Errorf("Operator: multiple operators can not be set"),
+		},
+		{
+			name: "happy path",
+			operator: Operator{
+				LessThanOrEqual: &OperatorValue{
+					FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+				},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.wantErr, tt.operator.validate())
+		})
+	}
+}
+
+func TestScore_validate(t *testing.T) {
+	tests := []struct {
+		name    string
+		score   TotalScore
+		wantErr error
+	}{
+		{
+			name: "happy path",
+			score: TotalScore{
+				PassPercentage:    90,
+				WarningPercentage: 80,
+			},
+			wantErr: nil,
+		},
+		{
+			name: "warn and pass equal",
+			score: TotalScore{
+				PassPercentage:    90,
+				WarningPercentage: 90,
+			},
+			wantErr: fmt.Errorf("Warn percentage score cannot be higher or equal than Pass percentage score"),
+		},
+		{
+			name: "warn higher than pass",
+			score: TotalScore{
+				PassPercentage:    90,
+				WarningPercentage: 95,
+			},
+			wantErr: fmt.Errorf("Warn percentage score cannot be higher or equal than Pass percentage score"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.wantErr, tt.score.validate())
+		})
+	}
+}
+
+func TestObjective_validate(t *testing.T) {
+	tests := []struct {
+		name    string
+		obj     Objective
+		wantErr error
+	}{
+		{
+			name:    "no Target set",
+			obj:     Objective{},
+			wantErr: nil,
+		},
+		{
+			name: "only warning set",
+			obj: Objective{
+				Target: Target{
+					Warning: &Operator{
+						EqualTo: &OperatorValue{
+							FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+						},
+					},
+				},
+			},
+			wantErr: fmt.Errorf("Warning criteria cannot be set without Failure criteria"),
+		},
+		{
+			name: "warning and failure set properly",
+			obj: Objective{
+				Target: Target{
+					Warning: &Operator{
+						EqualTo: &OperatorValue{
+							FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+						},
+					},
+					Failure: &Operator{
+						EqualTo: &OperatorValue{
+							FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+						},
+					},
+				},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.wantErr, tt.obj.validate())
+		})
+	}
+}
+
+func TestAnalysisDefinition_validateCreateUpdate(t *testing.T) {
+	tests := []struct {
+		name    string
+		obj     AnalysisDefinition
+		wantErr error
+	}{
+		{
+			name: "failure path - objective",
+			obj: AnalysisDefinition{
+				Spec: AnalysisDefinitionSpec{
+					Objectives: []Objective{
+						{
+							Target: Target{
+								Failure: &Operator{},
+							},
+						},
+					},
+				},
+			},
+			wantErr: fmt.Errorf("Operator: no operator set"),
+		},
+		{
+			name: "failure path - score",
+			obj: AnalysisDefinition{
+				Spec: AnalysisDefinitionSpec{
+					Objectives: []Objective{
+						{
+							Target: Target{
+								Failure: &Operator{
+									EqualTo: &OperatorValue{
+										FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+									},
+								},
+							},
+						},
+					},
+					TotalScore: TotalScore{
+						PassPercentage:    80,
+						WarningPercentage: 90,
+					},
+				},
+			},
+			wantErr: fmt.Errorf("Warn percentage score cannot be higher or equal than Pass percentage score"),
+		},
+		{
+			name: "happy path",
+			obj: AnalysisDefinition{
+				Spec: AnalysisDefinitionSpec{
+					Objectives: []Objective{
+						{
+							Target: Target{
+								Failure: &Operator{
+									EqualTo: &OperatorValue{
+										FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+									},
+								},
+							},
+						},
+					},
+					TotalScore: TotalScore{
+						PassPercentage:    80,
+						WarningPercentage: 70,
+					},
+				},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.wantErr, tt.obj.ValidateCreate())
+			require.Equal(t, tt.wantErr, tt.obj.ValidateUpdate(&AnalysisDefinition{}))
+		})
+	}
+}
+
+func TestTarget_validate(t *testing.T) {
+	tests := []struct {
+		name    string
+		target  Target
+		wantErr error
+	}{
+		{
+			name:    "neither Failure and Warning set",
+			target:  Target{},
+			wantErr: nil,
+		},
+		{
+			name: "Failure set",
+			target: Target{
+				Failure: &Operator{
+					LessThanOrEqual: &OperatorValue{
+						FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+					},
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "only warning set",
+			target: Target{
+				Warning: &Operator{
+					LessThanOrEqual: &OperatorValue{
+						FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+					},
+				},
+			},
+			wantErr: fmt.Errorf("Warning criteria cannot be set without Failure criteria"),
+		},
+		{
+			name:    "neither failure nor warning set",
+			target:  Target{},
+			wantErr: nil,
+		},
+		{
+			name: "only failure set",
+			target: Target{
+				Failure: &Operator{
+					EqualTo: &OperatorValue{
+						FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+					},
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "only warning set",
+			target: Target{
+				Warning: &Operator{
+					EqualTo: &OperatorValue{
+						FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+					},
+				},
+			},
+			wantErr: fmt.Errorf("Warning criteria cannot be set without Failure criteria"),
+		},
+		{
+			name: "warning not set properly",
+			target: Target{
+				Warning: &Operator{},
+				Failure: &Operator{
+					EqualTo: &OperatorValue{
+						FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+					},
+				},
+			},
+			wantErr: fmt.Errorf("Operator: no operator set"),
+		},
+		{
+			name: "warning and failure set properly",
+			target: Target{
+				Warning: &Operator{
+					EqualTo: &OperatorValue{
+						FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+					},
+				},
+				Failure: &Operator{
+					EqualTo: &OperatorValue{
+						FixedValue: *resource.NewQuantity(5, resource.DecimalSI),
+					},
+				},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.wantErr, tt.target.validate())
+		})
+	}
+}

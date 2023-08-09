87a3b22fab669b6ebf14f55f3ea162894c82f5b2
realanna <anna.reale@dynatrace.com>
2023-08-09 15:03:06 +0200

    chore: added new script
    Signed-off-by: realanna <anna.reale@dynatrace.com>

---
+++ .github/scripts/helm-test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Keptn Lifecycle Toolkit Helm Testing
+#
+# This script supports the comparison of standar values and expected templated results to helm chart
+# it is used to make sure changes to the chart are intentional and produce expected outcomes
+
+echo "running simple tests"
+  tests=$(find ./.github/scripts/.tests -maxdepth 1 -mindepth 1 -type d )
+
+  errors=0
+  successful=0
+
+  for test in $tests
+  do
+    helm template --namespace helmtests -f $test/values.yaml ./helm/chart > $test/helm_tests_output.yaml
+    if [ $? -ne 0 ]
+    then
+      echo "Error: helm template failed for test in $test"
+      errors=$((errors + 1))
+    else
+      diff "$test/helm_tests_output.yaml" "$test/result.yaml"
+      if [ $? -ne 0 ]
+      then
+        echo "Error: test in $test not successful"
+        errors=$((errors + 1))
+      else
+        echo "Info: test in $test successful"
+        successful=$((successful + 1))
+      fi
+    fi
+  done
+
+  echo "run $((errors + successful)) tests: successful $successful, errors $errors"
+  if [ $errors -gt 0 ]
+  then
+    exit 1
+  fi

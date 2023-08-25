fcfdcc84a4543a35575f546b4a7e0b599bb4b4a6
realanna <anna.reale@dynatrace.com>
2023-08-25 15:30:06 +0200

    feat(metrics-operator):  added evaluate test
    Signed-off-by: realanna <anna.reale@dynatrace.com>

--- metrics-operator/controllers/analysis/objectives_evaluator_test.go
+++ metrics-operator/controllers/analysis/objectives_evaluator_test.go
@@ -2,6 +2,7 @@ package analysis
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/go-logr/logr"
@@ -15,57 +16,107 @@ import (
 )
 
 func TestEvaluate(t *testing.T) {
-	fakeProvider := fake2.KeptnSLIProviderMock{
-		FetchAnalysisValueFunc: func(ctx context.Context, query string, spec metricsapi.AnalysisSpec, provider *metricsapi.KeptnMetricsProvider) (string, []byte, error) {
-			return "10", []byte{}, nil
-		},
-	}
-
-	evaluator := ObjectivesEvaluator{
-		Analysis: &metricsapi.Analysis{
-			Spec: metricsapi.AnalysisSpec{
-				Args: map[string]string{"a": "b"},
-				AnalysisDefinition: metricsapi.ObjectReference{
-					Name:      "a",
-					Namespace: "d",
+	// Define test cases
+	testCases := []struct {
+		name            string
+		providerType    string
+		mockProvider    providers.KeptnSLIProvider
+		providerRequest metricstypes.ProviderRequest
+		expectedResult  metricstypes.ProviderResult
+		expectedError   error
+	}{
+		{
+			name:         "SuccessfulEvaluation",
+			providerType: "mockProvider",
+			mockProvider: &fake2.KeptnSLIProviderMock{
+				FetchAnalysisValueFunc: func(ctx context.Context, query string, spec metricsapi.AnalysisSpec, provider *metricsapi.KeptnMetricsProvider) (string, []byte, error) {
+					return "10", []byte{}, nil
 				},
 			},
-		},
-
-		Client:  fake.NewClient(),
-		Log:     logr.Discard(),
-		results: make(chan metricstypes.ProviderResult, 1),
-		ProviderFactory: func(providerType string, log logr.Logger, k8sClient client.Client) (providers.KeptnSLIProvider, error) {
-			return &fakeProvider, nil
-		},
-	}
-
-	// Simulate context
-	ctx := context.TODO()
-
-	// Test with a valid provider type
-	providerType := "prometheus"
-	obj := make(chan metricstypes.ProviderRequest, 1)
-	go func() {
-		obj <- metricstypes.ProviderRequest{
-			Objective: &metricsapi.Objective{
-				AnalysisValueTemplateRef: metricsapi.ObjectReference{
+			providerRequest: metricstypes.ProviderRequest{
+				Objective: &metricsapi.Objective{
+					AnalysisValueTemplateRef: metricsapi.ObjectReference{
+						Name:      "mytemp",
+						Namespace: "default",
+					},
+				},
+				Query:    "query_fake_metric",
+				Provider: &metricsapi.KeptnMetricsProvider{Spec: metricsapi.KeptnMetricsProviderSpec{Type: "prometheus"}},
+			},
+			expectedResult: metricstypes.ProviderResult{
+				Objective: metricsapi.ObjectReference{
 					Name:      "mytemp",
 					Namespace: "default",
 				},
+				Value: "10",
+				Err:   nil,
 			},
-			Query:    "query_fake_metric",
-			Provider: &metricsapi.KeptnMetricsProvider{Spec: metricsapi.KeptnMetricsProviderSpec{Type: providerType}},
-		}
-		close(obj)
-	}()
-
-	evaluator.Evaluate(ctx, providerType, obj)
-	close(evaluator.results)
-	for res := range evaluator.results {
-		require.Equal(t, "mytemp", res.Objective.Name)
-		require.Contains(t, "10", res.Value)
-		require.Nil(t, res.Err)
+			expectedError: nil,
+		},
+		{
+			name:         "FailedEvaluation",
+			providerType: "mockProvider",
+			mockProvider: &fake2.KeptnSLIProviderMock{
+				FetchAnalysisValueFunc: func(ctx context.Context, query string, spec metricsapi.AnalysisSpec, provider *metricsapi.KeptnMetricsProvider) (string, []byte, error) {
+					return "", []byte{}, fmt.Errorf("something bad")
+				},
+			},
+			providerRequest: metricstypes.ProviderRequest{
+				Objective: &metricsapi.Objective{
+					AnalysisValueTemplateRef: metricsapi.ObjectReference{
+						Name:      "mytemp",
+						Namespace: "default",
+					},
+				},
+				Query:    "query_fake_metric",
+				Provider: &metricsapi.KeptnMetricsProvider{Spec: metricsapi.KeptnMetricsProviderSpec{Type: "prometheus"}},
+			},
+			expectedResult: metricstypes.ProviderResult{
+				Objective: metricsapi.ObjectReference{
+					Name:      "mytemp",
+					Namespace: "default",
+				},
+				Value: "",
+				Err:   fmt.Errorf("something bad"),
+			},
+			expectedError: fmt.Errorf("something bad"),
+		},
 	}
 
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			mockProviderFactory := func(providerType string, log logr.Logger, client client.Client) (providers.KeptnSLIProvider, error) {
+				// Define your mock provider implementation
+				return tc.mockProvider, nil
+			}
+			objectivesEvaluator := ObjectivesEvaluator{
+				// Initialize ObjectivesEvaluator fields here
+				Client:          fake.NewClient(),
+				Log:             logr.Discard(),
+				ProviderFactory: mockProviderFactory,
+				Analysis: &metricsapi.Analysis{
+					Spec: metricsapi.AnalysisSpec{
+						AnalysisDefinition: metricsapi.ObjectReference{
+							Name:      "a",
+							Namespace: "d",
+						},
+					},
+				},
+				results: make(chan metricstypes.ProviderResult, 1),
+			}
+
+			ctx := context.TODO()
+			objChan := make(chan metricstypes.ProviderRequest, 1)
+			go func() {
+				objChan <- tc.providerRequest
+				close(objChan)
+			}()
+			objectivesEvaluator.Evaluate(ctx, tc.providerType, objChan)
+			close(objectivesEvaluator.results)
+			result := <-objectivesEvaluator.results
+
+			require.Equal(t, tc.expectedResult, result)
+			require.Equal(t, tc.expectedError, result.Err)
+		})
+	}
 }

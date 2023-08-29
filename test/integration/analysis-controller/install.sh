cbeb6fd6121537a30788e275e9696e911a7ec68d
realanna <anna.reale@dynatrace.com>
2023-08-29 08:48:22 +0200

    feat(metrics-operator): fix integration test
    Signed-off-by: realanna <anna.reale@dynatrace.com>

--- test/integration/analysis-controller/install.sh
+++ test/integration/analysis-controller/install.sh
@@ -6,8 +6,8 @@ new_timestamp=$((initial_timestamp + seconds_to_add))
 time_20_seconds_later=$(date -u -d "@$new_timestamp" +"%Y-%m-%dT%H:%M:%SZ")
 export CURRENT_TIME="$current_time"
 export LATER="$time_20_seconds_later"
+sed -i 's/from: .*$/from: $CURRENT_TIME/g; s/to: .*$/to: $LATER/g' 01-install.yaml
 echo "templating time" $CURRENT_TIME $LATER
-cp "01-install.yaml" tmp
 envsubst <"01-install.yaml" >out.yaml
 rm "01-install.yaml"
 mv out.yaml "01-install.yaml"

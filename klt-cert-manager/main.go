a97f586dbf23db412182524ae048fc37a83c71d3
geoffrey1330 <israelgeoffrey13@gmail.com>
2023-09-18 11:46:05 +0100

    Added prepopulated keptn_ metrics to localhost:2222/metrics endpoint Signed-off-by: geoffrey1330 <israelgeoffrey13@gmail.com>

--- klt-cert-manager/main.go
+++ klt-cert-manager/main.go
@@ -13,14 +13,12 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
 	// nolint:gci
 	// +kubebuilder:scaffold:imports
 	"go.opentelemetry.io/otel/exporters/prometheus"

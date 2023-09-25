ee998d578ca4ecfeb0a215719abfea77b062f4f6
geoffrey1330 <israelgeoffrey13@gmail.com>
2023-09-25 10:13:28 +0100

    Add prepopulated keptn_ metrics to localhost:2222/metrics endpoint Signed-off-by: geoffrey1330 <israelgeoffrey13@gmail.com>

--- test/testmetrics/metrics/retrieve-metrics2.sh
+++ test/testmetrics/metrics/retrieve-metrics2.sh
@@ -4,20 +4,11 @@
 
 component_name=$1
 
-# Use kubectl port-forward to forward local port 2222 to the metrics service
-kubectl -n keptn-lifecycle-toolkit-system port-forward service/lifecycle-operator-metrics-service 2222 &
-
-# Sleep for a moment to allow port-forwarding to be established
-sleep 2
-
 # Metrics URL is now localhost:2222
-metrics_url="http://localhost:2222/metrics"
+metrics_url="http://lifecycle-operator-metrics-service.keptn-lifecycle-toolkit-system.svc.cluster.local:2222/metrics"
 
 # Fetch metrics for the specified component
 metrics=$(curl -s $metrics_url | grep "${component_name}_active")
 
 echo "Metrics for $component_name:"
 echo "$metrics"
-
-# Kill the port-forwarding process
-pkill -f "kubectl -n keptn-lifecycle-toolkit-system port-forward"

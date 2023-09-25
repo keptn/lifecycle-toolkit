34fcf45768e44d8b8fc4fbc7819369e66ab7141f
realanna <anna.reale@dynatrace.com>
2023-09-25 08:28:12 +0200

    feat(metrics-operator): revert change to ddprovider
    Signed-off-by: realanna <anna.reale@dynatrace.com>

--- metrics-operator/controllers/common/providers/datadog/common.go
+++ metrics-operator/controllers/common/providers/datadog/common.go
@@ -8,14 +8,25 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"strings"
 )
 
 const apiKey, appKey = "DD_CLIENT_API_KEY", "DD_CLIENT_APP_KEY"
 
 var ErrSecretKeyRefNotDefined = errors.New("the SecretKeyRef property with the DataDog API Key is missing")
 
+func hasDDSecretDefined(spec metricsapi.KeptnMetricsProviderSpec) bool {
+	if spec.SecretKeyRef == (corev1.SecretKeySelector{}) {
+		return false
+	}
+	if strings.TrimSpace(spec.SecretKeyRef.Name) == "" {
+		return false
+	}
+	return true
+}
+
 func getDDSecret(ctx context.Context, provider metricsapi.KeptnMetricsProvider, k8sClient client.Client) (string, string, error) {
-	if !provider.HasSecretDefined() {
+	if !hasDDSecretDefined(provider.Spec) {
 		return "", "", ErrSecretKeyRefNotDefined
 	}
 	ddCredsSecret := &corev1.Secret{}

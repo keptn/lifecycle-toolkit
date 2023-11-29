4580cc158ecb20c6e2d519a21cd7ba0ac1efb7fd
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2023-11-29 09:57:02 +0100

    use lifecycle.keptn.sh for the version announcement
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- docs-new/assets/javascripts/versions.js
+++ docs-new/assets/javascripts/versions.js
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded", function() {
     var margin = 30;
     var headerHeight = document.getElementsByClassName("md-header")[0].offsetHeight;
     if (rtdData.version === "latest") {
-        document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for an unreleased version of Keptn, <a href='https://keptn.sh/en/stable/'>click here to go to the latest stable version.</a></div>"
+        document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for an unreleased version of Keptn, <a href='https://lifecycle.keptn.sh/'>click here to go to the latest stable version.</a></div>"
         var bannerHeight = document.getElementById('announce-msg').offsetHeight + margin
         document.querySelector("header.md-header").style.top = bannerHeight +"px";
         document.querySelector('style').textContent +=
@@ -13,7 +13,7 @@ window.addEventListener("DOMContentLoaded", function() {
             "@media screen and (min-width: 60em){ .md-sidebar--secondary { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
     }
     else if (rtdData.version !== "stable") {
-        document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for a previous version of Keptn, <a href='https://keptn.sh/en/stable/'>click here to go to the latest stable version.</a></div>"
+        document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for a previous version of Keptn, <a href='https://lifecycle.keptn.sh/'>click here to go to the latest stable version.</a></div>"
         var bannerHeight = document.getElementById('announce-msg').offsetHeight + margin
         document.querySelector("header.md-header").style.top = bannerHeight +"px";
         document.querySelector('style').textContent +=

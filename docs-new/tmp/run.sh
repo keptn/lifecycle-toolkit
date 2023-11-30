34bafa6094cd8174a49b52a95a9783d6eb51389f
Giovanni Liva <giovanni.liva@dynatrace.com>
2023-11-30 09:11:42 +0100

    remove tmp
    Signed-off-by: Giovanni Liva <giovanni.liva@dynatrace.com>

--- docs-new/tmp/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-for i in $(find $1 -name "*.md" -type f); do
-    [ -f "$i" ] || break
-    #echo $i
-    go run ${DIR}/main.go $i
-done

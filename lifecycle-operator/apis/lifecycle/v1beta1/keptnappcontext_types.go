142a5b9ae918f9844b2f6a45c96d3a2ac05208ad
geoffrey1330 <israelgeoffrey13@gmail.com>
2024-01-08 10:37:21 +0100

    Add KeptnAppContext Crd in lifecycle-operator Signed-off-by: geoffrey1330 <israelgeoffrey13@gmail.com>

---
+++ lifecycle-operator/apis/lifecycle/v1beta1/keptnappcontext_types.go
@@ -0,0 +1,89 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+type DeploymentTaskSpec struct {
+	// PreDeploymentTasks is a list of all tasks to be performed during the pre-deployment phase of the KeptnApp.
+	// The items of this list refer to the names of KeptnTaskDefinitions
+	// located in the same namespace as the KeptnApp, or in the Keptn namespace.
+	PreDeploymentTasks []string `json:"preDeploymentTasks,omitempty"`
+	// PostDeploymentTasks is a list of all tasks to be performed during the post-deployment phase of the KeptnApp.
+	// The items of this list refer to the names of KeptnTaskDefinitions
+	// located in the same namespace as the KeptnApp, or in the Keptn namespace.
+	PostDeploymentTasks []string `json:"postDeploymentTasks,omitempty"`
+	// PreDeploymentEvaluations is a list of all evaluations to be performed
+	// during the pre-deployment phase of the KeptnApp.
+	// The items of this list refer to the names of KeptnEvaluationDefinitions
+	// located in the same namespace as the KeptnApp, or in the Keptn namespace.
+	PreDeploymentEvaluations []string `json:"preDeploymentEvaluations,omitempty"`
+	// PostDeploymentEvaluations is a list of all evaluations to be performed
+	// during the post-deployment phase of the KeptnApp.
+	// The items of this list refer to the names of KeptnEvaluationDefinitions
+	// located in the same namespace as the KeptnApp, or in the Keptn namespace.
+	PostDeploymentEvaluations []string `json:"postDeploymentEvaluations,omitempty"`
+}
+
+// KeptnAppContextSpec defines the desired state of KeptnAppContext
+type KeptnAppContextSpec struct {
+	DeploymentTaskSpec `json:",inline"`
+
+	// +optional
+	// Metadata contains additional key-value pairs for contextual information.
+	Metadata map[string]string `json:"metadata,omitempty"`
+
+	// +optional
+	// TraceLinks are links to trace IDs for tracking.
+	TraceLinks []string `json:"traceLinks,omitempty"`
+}
+
+// KeptnAppContextStatus defines the observed state of KeptnAppContext
+type KeptnAppContextStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// KeptnAppContext is the Schema for the keptnappcontexts API
+type KeptnAppContext struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KeptnAppContextSpec   `json:"spec,omitempty"`
+	Status KeptnAppContextStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// KeptnAppContextList contains a list of KeptnAppContext
+type KeptnAppContextList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []KeptnAppContext `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&KeptnAppContext{}, &KeptnAppContextList{})
+}

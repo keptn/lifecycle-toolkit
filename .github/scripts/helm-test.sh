4f3d1e86e7c3ed712d23563bd754c213f317c28c
odubajDT <ondrej.dubaj@dynatrace.com>
2024-01-09 11:45:18 +0100

    revert helm_tests
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- .github/scripts/helm-test.sh
+++ .github/scripts/helm-test.sh
@@ -31,29 +31,29 @@ echo "running Helm tests"
   for test in $tests
   do
     echo "Testing $test"
-    helm template keptn-test --namespace helmtests -f $test/values.yaml ./chart > $test/result.yaml
-    # if [ $? -ne 0 ]
-    # then
-    #   echo "Error: helm template failed for test in $test"
-    #   errors=$((errors + 1))
-    #   failures+="\n test $test failed"
-    # else
-    #   diff -u "$test/helm_tests_output.yaml" "$test/result.yaml"
-    #   if [ $? -ne 0 ]
-    #   then
-    #     echo "Error: test in $test not successful"
-    #     errors=$((errors + 1))
-    #     failures+="\n test $test failed"
-    #   else
-    #     echo "Info: test in $test successful"
-    #     successful=$((successful + 1))
-    #   fi
-    # fi
+    helm template keptn-test --namespace helmtests -f $test/values.yaml ./chart > $test/helm_tests_output.yaml
+    if [ $? -ne 0 ]
+    then
+      echo "Error: helm template failed for test in $test"
+      errors=$((errors + 1))
+      failures+="\n test $test failed"
+    else
+      diff -u "$test/helm_tests_output.yaml" "$test/result.yaml"
+      if [ $? -ne 0 ]
+      then
+        echo "Error: test in $test not successful"
+        errors=$((errors + 1))
+        failures+="\n test $test failed"
+      else
+        echo "Info: test in $test successful"
+        successful=$((successful + 1))
+      fi
+    fi
   done
 
-  # echo "run $((errors + successful)) tests: successful $successful, errors $errors"
-  # echo -e $failures
-  # if [ $errors -gt 0 ]
-  # then
-  #   exit 1
-  # fi
+  echo "run $((errors + successful)) tests: successful $successful, errors $errors"
+  echo -e $failures
+  if [ $errors -gt 0 ]
+  then
+    exit 1
+  fi

27012384458a65a2dc0957c37d9de246d4813c43
Dubaj <ondrej.dubaj@dt-npq902mgtx.clients.dynatrace.org>
2024-01-10 08:54:56 +0100

    add conversion webhooks
    Signed-off-by: Dubaj <ondrej.dubaj@dt-npq902mgtx.clients.dynatrace.org>

---
+++ lifecycle-operator/apis/lifecycle/v1alpha3/keptnapp_conversion.go
@@ -0,0 +1,71 @@
+package v1alpha3
+
+import (
+	"fmt"
+
+	"github.com/keptn/lifecycle-toolkit/lifecycle-operator/apis/lifecycle/v1alpha3/common"
+	"github.com/keptn/lifecycle-toolkit/lifecycle-operator/apis/lifecycle/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/conversion"
+)
+
+// ConvertTo converts the src v1alpha3.KeptnApp to the hub version (v1beta1.KeptnApp)
+func (src *KeptnApp) ConvertTo(dstRaw conversion.Hub) error {
+	dst, ok := dstRaw.(*v1beta1.KeptnApp)
+
+	if !ok {
+		return fmt.Errorf("type %T %w", dstRaw, common.ErrCannotCastKeptnApp)
+	}
+
+	// Copy equal stuff to new object
+	// DO NOT COPY TypeMeta
+	dst.ObjectMeta = src.ObjectMeta
+
+	dst.Spec.Version = src.Spec.Version
+	for _, srcWl := range src.Spec.Workloads {
+		dst.Spec.Workloads = append(dst.Spec.Workloads, v1beta1.KeptnWorkloadRef{
+			Name:    srcWl.Name,
+			Version: srcWl.Version,
+		})
+	}
+	dst.Spec.PreDeploymentTasks = src.Spec.PreDeploymentTasks
+	dst.Spec.PostDeploymentTasks = src.Spec.PostDeploymentTasks
+	dst.Spec.PreDeploymentEvaluations = src.Spec.PreDeploymentEvaluations
+	dst.Spec.PostDeploymentEvaluations = src.Spec.PostDeploymentEvaluations
+
+	dst.Status.CurrentVersion = src.Status.CurrentVersion
+
+	dst.Spec.Revision = src.Spec.Revision
+
+	return nil
+}
+
+// ConvertFrom converts from the hub version (v1beta1.KeptnApp) to this version (v1alpha3.KeptnApp)
+func (dst *KeptnApp) ConvertFrom(srcRaw conversion.Hub) error {
+	src, ok := srcRaw.(*v1beta1.KeptnApp)
+
+	if !ok {
+		return fmt.Errorf("type %T %w", srcRaw, common.ErrCannotCastKeptnApp)
+	}
+
+	// Copy equal stuff to new object
+	// DO NOT COPY TypeMeta
+	dst.ObjectMeta = src.ObjectMeta
+
+	dst.Spec.Version = src.Spec.Version
+	for _, srcWl := range src.Spec.Workloads {
+		dst.Spec.Workloads = append(dst.Spec.Workloads, KeptnWorkloadRef{
+			Name:    srcWl.Name,
+			Version: srcWl.Version,
+		})
+	}
+	dst.Spec.PreDeploymentTasks = src.Spec.PreDeploymentTasks
+	dst.Spec.PostDeploymentTasks = src.Spec.PostDeploymentTasks
+	dst.Spec.PreDeploymentEvaluations = src.Spec.PreDeploymentEvaluations
+	dst.Spec.PostDeploymentEvaluations = src.Spec.PostDeploymentEvaluations
+
+	dst.Status.CurrentVersion = src.Status.CurrentVersion
+
+	dst.Spec.Revision = src.Spec.Revision
+
+	return nil
+}

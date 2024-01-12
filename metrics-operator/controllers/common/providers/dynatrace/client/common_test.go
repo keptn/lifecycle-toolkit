b77191cfa8d4aec4942cd12fdc6791b25c48d5ce
Prakriti Mandal <98270250+prakrit55@users.noreply.github.com>
2024-01-12 19:19:01 +0530

    chore(metrics-operator): make Dynatrace DQL provider oAuth URL configurable (#2713)
    Signed-off-by: Griffin <prakritimandal611@gmail.com>
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    Co-authored-by: Florian Bacher <florian.bacher@dynatrace.com>

--- metrics-operator/controllers/common/providers/dynatrace/client/common_test.go
+++ metrics-operator/controllers/common/providers/dynatrace/client/common_test.go
@@ -3,47 +3,7 @@ package client
 import (
 	"net/http"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
-
-func Test_validateOAuthSecret(t *testing.T) {
-	tests := []struct {
-		name   string
-		input  string
-		result error
-	}{
-		{
-			name:   "good token",
-			input:  "dt0s08.XX.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-			result: nil,
-		},
-		{
-			name:   "wrong prefix",
-			input:  "",
-			result: ErrClientSecretInvalid,
-		},
-		{
-			name:   "wrong format",
-			input:  "",
-			result: ErrClientSecretInvalid,
-		},
-		{
-			name:   "wrong secret part",
-			input:  "",
-			result: ErrClientSecretInvalid,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := validateOAuthSecret(tt.input)
-
-			require.ErrorIs(t, err, tt.result)
-		})
-
-	}
-}
 
 func Test_isErrorStatus(t *testing.T) {
 	type args struct {
--- metrics-operator/controllers/common/providers/dynatrace/common.go
+++ metrics-operator/controllers/common/providers/dynatrace/common.go
@@ -2,6 +2,7 @@ package dynatrace
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/url"
@@ -16,12 +17,47 @@ import (
 var ErrSecretKeyRefNotDefined = errors.New("the SecretKeyRef property with the Dynatrace token is missing")
 var ErrInvalidResult = errors.New("the answer does not contain any data")
 var ErrDQLQueryTimeout = errors.New("timed out waiting for result of DQL query")
+var ErrInvalidAuthURL = errors.New("the Dynatrace auth URL is not a valid URL")
+var ErrInvalidToken = errors.New("the Dynatrace token has an invalid format")
 
-const ErrAPIMsg = "provider api response: %s"
+const (
+	ErrAPIMsg     = "provider api response: %s"
+	dtTokenPrefix = "dt0s08"
+)
 
 type Error struct {
 	Code    int    `json:"-"` // optional
 	Message string `json:"message"`
+}
+
+type DQLSecret struct {
+	Token   string `json:"token"`
+	AuthUrl string `json:"authUrl"`
+}
+
+func (s DQLSecret) validate() error {
+	// must start with dt0s08
+	// must have 2 dots
+	// third part (split by dot) must be 64 chars
+	if !strings.HasPrefix(s.Token, dtTokenPrefix) {
+		return fmt.Errorf("secret does not start with required prefix %s: %w", dtTokenPrefix, ErrInvalidToken)
+	}
+	split := strings.Split(s.Token, ".")
+	if len(split) != 3 {
+		return fmt.Errorf("secret does not contain three components: %w", ErrInvalidToken)
+	}
+	secret := split[2]
+	if secretLen := len(secret); secretLen != 64 {
+		return fmt.Errorf("length of secret is %d, which is not equal to 64: %w", secretLen, ErrInvalidToken)
+	}
+
+	if s.AuthUrl != "" {
+		if _, err := url.ParseRequestURI(s.AuthUrl); err != nil {
+			return fmt.Errorf("authurl is not a valid url: %w", ErrInvalidAuthURL)
+		}
+	}
+
+	return nil
 }
 
 func getDTSecret(ctx context.Context, provider metricsapi.KeptnMetricsProvider, k8sClient client.Client) (string, error) {
@@ -38,6 +74,30 @@ func getDTSecret(ctx context.Context, provider metricsapi.KeptnMetricsProvider, 
 		return "", fmt.Errorf("secret contains invalid key %s", provider.Spec.SecretKeyRef.Key)
 	}
 	return strings.Trim(token, "\n"), nil
+}
+
+func getDQLSecret(ctx context.Context, provider metricsapi.KeptnMetricsProvider, k8sClient client.Client) (*DQLSecret, error) {
+	if !provider.HasSecretDefined() || !provider.HasSecretKeyDefined() {
+		return nil, ErrSecretKeyRefNotDefined
+	}
+	dtCredsSecret := &corev1.Secret{}
+	if err := k8sClient.Get(ctx, types.NamespacedName{Name: provider.Spec.SecretKeyRef.Name, Namespace: provider.Namespace}, dtCredsSecret); err != nil {
+		return nil, err
+	}
+
+	credentialsStr := string(dtCredsSecret.Data[provider.Spec.SecretKeyRef.Key])
+
+	credentialsObj := &DQLSecret{}
+
+	if err := json.Unmarshal([]byte(credentialsStr), credentialsObj); err != nil {
+		return nil, fmt.Errorf("could not unmarshal secret containing access credentials: %w", err)
+	}
+
+	if err := credentialsObj.validate(); err != nil {
+		return nil, fmt.Errorf("secret contains invalid credentials: %w", err)
+	}
+
+	return credentialsObj, nil
 }
 
 func urlEncodeQuery(query string) string {

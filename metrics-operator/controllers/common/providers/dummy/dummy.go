dcd8f513a03fad1904c2be2c2945ad5e3dff8d6f
geoffrey1330 <israelgeoffrey13@gmail.com>
2024-02-06 10:15:49 +0100

    Added dummy provider in keptn metric provider Signed-off-by: geoffrey1330 <israelgeoffrey13@gmail.com>

--- metrics-operator/controllers/common/providers/dummy/dummy.go
+++ metrics-operator/controllers/common/providers/dummy/dummy.go
@@ -16,7 +16,7 @@ type KeptnDummyProvider struct {
 }
 
 func (d *KeptnDummyProvider) FetchAnalysisValue(ctx context.Context, query string, analysis metricsapi.Analysis, provider *metricsapi.KeptnMetricsProvider) (string, error) {
-	return fmt.Sprintf("dummy provider FetchAnalysisValue was called with query %s from %q to %q", query, analysis.GetFrom().Unix(), analysis.GetTo().Unix()), nil
+	return fmt.Sprintf("dummy provider FetchAnalysisValue was called with query %s from %d to %d", query, analysis.GetFrom().Unix(), analysis.GetTo().Unix()), nil
 }
 
 func (d *KeptnDummyProvider) EvaluateQuery(ctx context.Context, metric metricsapi.KeptnMetric, provider metricsapi.KeptnMetricsProvider) (string, []byte, error) {
@@ -28,7 +28,7 @@ func (d *KeptnDummyProvider) EvaluateQueryForStep(ctx context.Context, metric me
 	if err != nil {
 		return nil, nil, err
 	}
-	result := fmt.Sprintf("dummy provider EvaluateQueryForStep was called with query %s from %q to %q at an interval %q", metric.Spec.Query, fromTime, toTime, stepInterval)
+	result := fmt.Sprintf("dummy provider EvaluateQueryForStep was called with query %s from %d to %d at an interval %d", metric.Spec.Query, fromTime, toTime, stepInterval)
 	return []string{result}, nil, nil
 }
 

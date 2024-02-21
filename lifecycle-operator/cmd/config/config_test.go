79f984615ca1d33742ba5df4e45fa6372e583ea6
geoffrey1330 <israelgeoffrey13@gmail.com>
2024-02-21 11:28:55 +0100

    Increased unit test coverage of the lifecycle operator Signed-off-by: geoffrey1330 <israelgeoffrey13@gmail.com>

--- lifecycle-operator/cmd/config/config_test.go
+++ lifecycle-operator/cmd/config/config_test.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,28 +13,10 @@ func TestConfigProvider(t *testing.T) {
 	})
 
 	t.Run("GetConfigSuccess", func(t *testing.T) {
-
-		tempDir := setupMockedKubeConfig(t)
-		defer os.RemoveAll(tempDir)
-
 		provider := NewKubeConfigProvider()
 
 		config, err := provider.GetConfig()
 		assert.NoError(t, err)
 		assert.NotNil(t, config)
 	})
-}
-
-func setupMockedKubeConfig(t *testing.T) string {
-	tempDir := t.TempDir()
-
-	kubeConfigFile := filepath.Join(tempDir, ".kube", "config")
-	err := os.MkdirAll(filepath.Dir(kubeConfigFile), 0755)
-	assert.NoError(t, err)
-	err = os.WriteFile(kubeConfigFile, []byte("mocked kubeconfig content"), 0644)
-	assert.NoError(t, err)
-
-	os.Setenv("HOME", tempDir)
-
-	return tempDir
 }

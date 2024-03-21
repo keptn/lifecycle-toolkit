d9ebd05f6b0efde4765c02455f0158dfe912ff6c
odubajDT <ondrej.dubaj@dynatrace.com>
2024-03-21 11:19:17 +0100

    chore(metrics-operator): cleanup APIs
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- metrics-operator/api/v1beta1/keptnmetricsprovider_types.go
+++ metrics-operator/api/v1beta1/keptnmetricsprovider_types.go
@@ -17,8 +17,6 @@ limitations under the License.
 package v1beta1
 
 import (
-	"strings"
-
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -66,33 +64,4 @@ type KeptnMetricsProviderList struct {
 
 func init() {
 	SchemeBuilder.Register(&KeptnMetricsProvider{}, &KeptnMetricsProviderList{})
-}
-
-func (p *KeptnMetricsProvider) HasSecretDefined() bool {
-	if p.Spec.SecretKeyRef == (corev1.SecretKeySelector{}) {
-		return false
-	}
-	//if the secret name exists the secret is defined
-	if strings.TrimSpace(p.Spec.SecretKeyRef.Name) == "" {
-		return false
-	}
-	return true
-}
-
-func (p *KeptnMetricsProvider) HasSecretKeyDefined() bool {
-	if p.Spec.SecretKeyRef == (corev1.SecretKeySelector{}) {
-		return false
-	}
-	//if the secret name exists the secret is defined
-	if strings.TrimSpace(p.Spec.SecretKeyRef.Key) == "" {
-		return false
-	}
-	return true
-}
-
-func (p *KeptnMetricsProvider) GetType() string {
-	if p.Spec.Type != "" {
-		return p.Spec.Type
-	}
-	return p.Name
 }

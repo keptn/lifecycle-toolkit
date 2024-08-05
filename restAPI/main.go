d81a83c321c0029aa1cd686261fd7e3356b85658
asamonik <49288593+asamonik@users.noreply.github.com>
2024-08-05 09:40:04 +0200

    metricscount
    Signed-off-by: asamonik <49288593+asamonik@users.noreply.github.com>

--- restAPI/main.go
+++ restAPI/main.go
@@ -2,53 +2,69 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
+	"path/filepath"
+
 	"fmt"
 	"log"
 	"net/http"
-	"path/filepath"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+
+	metrics_v1 "github.com/keptn/lifecycle-toolkit/metrics-operator/api/v1"
 )
 
 var clientset *kubernetes.Clientset
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func handlerMetricsCount(w http.ResponseWriter, r *http.Request) {
 	log.Println("http call to /pods")
 
-	pods, err := clientset.CoreV1().Pods("default").List(context.TODO(), v1.ListOptions{})
+	d, err := clientset.RESTClient().
+		Get().
+		AbsPath("/apis/metrics.keptn.sh/v1/").
+		Resource("keptnmetrics").
+		Namespace("default").
+		DoRaw(context.TODO())
+
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 	}
 
+	l := metrics_v1.KeptnMetricList{}
+
+	err = json.Unmarshal([]byte(d), &l)
+
 	w.WriteHeader(http.StatusOK)
-	returnval := fmt.Sprintf("%#v", pods)
+	returnval := fmt.Sprintf("%#v", len(l.Items))
 	w.Write([]byte(returnval))
 }
 
 func main() {
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		kubeconfig = flag.String("kc", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		kubeconfig = flag.String("kc", "", "absolute path to the kubeconfig file")
 	}
 	flag.Parse()
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+
+	clientConfig, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err = kubernetes.NewForConfig(clientConfig)
 
 	if err != nil {
 		panic(err)
 	}
 
-	clientset, err = kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err)
-	}
-
-	http.Handle("/pods", http.HandlerFunc(handlerFunc))
+	http.Handle("/metricscount", http.HandlerFunc(handlerMetricsCount))
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Listening on port 8080")
 }

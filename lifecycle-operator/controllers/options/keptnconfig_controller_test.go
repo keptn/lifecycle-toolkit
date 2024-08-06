b43d6f8f6ed63129b5e1816a6b80d45df7505860
asamonik <49288593+asamonik@users.noreply.github.com>
2024-08-06 14:10:35 +0200

    removed unnecessary stuff
    Signed-off-by: asamonik <49288593+asamonik@users.noreply.github.com>

--- lifecycle-operator/controllers/options/keptnconfig_controller_test.go
+++ lifecycle-operator/controllers/options/keptnconfig_controller_test.go
@@ -17,9 +17,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 )
 
 func TestKeptnConfigReconciler_Reconcile(t *testing.T) {
@@ -485,23 +482,7 @@ func TestKeptnConfigReconciler_reconcileRestApiEnabled(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			reconcileConfig := &optionsv1alpha1.KeptnConfig{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "empty-config",
-					Namespace: "keptn-system",
-				},
-				Spec: optionsv1alpha1.KeptnConfigSpec{
-					OTelCollectorUrl: "",
-					BlockDeployment:  true,
-					RestApiEnabled:   false,
-					ObservabilityTimeout: metav1.Duration{
-						Duration: time.Duration(5 * time.Minute),
-					},
-				},
-			}
-
-			r := setupReconciler(reconcileConfig)
-
+			r := setupReconciler(tt.args.config)
 			got, err := r.reconcileRestApi(tt.args.config)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("reconcileRestApi() error = %v, wantErr %v", err, tt.wantErr)
@@ -523,17 +504,9 @@ func setupReconciler(withConfig *optionsv1alpha1.KeptnConfig) *KeptnConfigReconc
 
 	fakeClient := testcommon.NewTestClient(withConfig)
 
-	schemes := runtime.NewSchemeBuilder(
-		appsv1.AddToScheme,
-		corev1.AddToScheme,
-	)
-
-	s := runtime.NewScheme()
-	schemes.AddToScheme(s)
-
 	r := NewReconciler(
 		fakeClient,
-		s,
+		fakeClient.Scheme(),
 		ctrl.Log.WithName("test-keptnconfig-controller"),
 	)
 	r.config = &fakeconfig.MockConfig{
